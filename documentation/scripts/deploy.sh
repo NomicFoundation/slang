ee2c7f5b31fa07df760822655bb4b50d763e9fdd
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2022-04-15 16:58:58 -0700

    documentation structure (#16)

---
+++ documentation/scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+PROJECT_DIR=$(dirname "$THIS_DIR")
+TARGET_DIR="$PROJECT_DIR/target"
+
+# shellcheck source=/dev/null
+[[ "${HERMIT_ENV:-}" == "$PROJECT_DIR" ]] || source "$PROJECT_DIR/bin/activate-hermit"
+
+(
+  # Perform a full build first
+  "$THIS_DIR/build.sh"
+)
+
+if [ "$GITHUB_REF" != "refs/heads/main" ] || [ "$GITHUB_EVENT_NAME" != "push" ]; then
+  printf "\n\n❌ Deployment environment not detected: Aborting deployment ❌\n\n"
+  exit 1
+fi
+
+(
+  printf "\n\n🚀 Deploy to GitHub Pages 🚀\n\n\n"
+  cd "$PROJECT_DIR"
+  python3 -m pipenv run ghp-import --no-jekyll --no-history --push "$TARGET_DIR"
+)
+
+printf "\n\n✅ Workflow Success ✅\n\n\n"
