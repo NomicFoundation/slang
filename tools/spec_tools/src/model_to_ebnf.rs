f1116bfd865c7bf2f0adb5373daaf8f05bfb3ca3
AntonyBlakey <antony.blakey@gmail.com>
2022-05-09 15:57:08 +0200

    Generate ebnf with precedence

--- tools/spec_tools/src/model_to_ebnf.rs
+++ tools/spec_tools/src/model_to_ebnf.rs
@@ -1,7 +1,13 @@
 use crate::model::*;
 
 pub fn generate(productions: &Grammar) {
+    let mut first = true;
     for (name, expr) in productions {
+        if first {
+            first = false;
+        } else {
+            println!();
+        }
         print!("{} =", name);
         expr.generate_ebnf();
         println!(" ;")
@@ -9,6 +15,32 @@ pub fn generate(productions: &Grammar) {
 }
 
 impl Expression {
+    fn precedence_ebnf(&self) -> u8 {
+        match self {
+            Expression::End { .. } => 0,
+            Expression::Any { .. } => 0,
+            Expression::Repeated { .. } => 0,
+            Expression::Optional { .. } => 0,
+            Expression::Negation { .. } => 1,
+            Expression::Choice { .. } => 4,
+            Expression::Sequence { .. } => 3,
+            Expression::Difference { .. } => 2,
+            Expression::Chars { .. } => 0,
+            Expression::Identifier { .. } => 0,
+            Expression::CharRange { .. } => 0,
+        }
+    }
+
+    fn generate_ebnf_subexpression(&self, expr: &ExpressionRef) {
+        if self.precedence_ebnf() < expr.precedence_ebnf() {
+            print!(" (");
+            expr.generate_ebnf();
+            print!(" )")
+        } else {
+            expr.generate_ebnf();
+        }
+    }
+
     fn generate_ebnf(&self) {
         match self {
             Expression::End { .. } => print!(" $"),
@@ -17,7 +49,7 @@ impl Expression {
 
             Expression::Repeated { expr, .. } => {
                 print!(" {{");
-                expr.generate_ebnf();
+                self.generate_ebnf_subexpression(expr);
                 print!(" }}");
             }
 
@@ -29,11 +61,10 @@ impl Expression {
 
             Expression::Negation { expr, .. } => {
                 print!(" ¬");
-                expr.generate_ebnf();
+                self.generate_ebnf_subexpression(expr);
             }
 
             Expression::Choice { exprs, .. } => {
-                print!(" ");
                 let mut first = true;
                 for expr in exprs {
                     if first {
@@ -41,13 +72,13 @@ impl Expression {
                     } else {
                         print!(" |");
                     }
-                    expr.generate_ebnf();
+                    self.generate_ebnf_subexpression(expr);
                 }
             }
 
             Expression::Sequence { exprs, .. } => {
                 for expr in exprs {
-                    expr.generate_ebnf();
+                    self.generate_ebnf_subexpression(expr);
                 }
             }
 
@@ -56,19 +87,22 @@ impl Expression {
                 subtrahend,
                 ..
             } => {
-                minuend.generate_ebnf();
+                self.generate_ebnf_subexpression(minuend);
                 print!(" -");
-                subtrahend.generate_ebnf();
-            }
-            Expression::Chars { string, .. } => {
-                print!(" {:?}", string);
+                self.generate_ebnf_subexpression(subtrahend);
             }
 
             Expression::Identifier { name, .. } => {
                 print!(" {}", name);
             }
 
+            Expression::Chars { string, .. } => {
+                // TODO: encode properly
+                print!(" {:?}", string);
+            }
+
             Expression::CharRange { start, end, .. } => {
+                // TODO: encode properly
                 print!(" {:?} … {:?}", start, end);
             }
         }
