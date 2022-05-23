299e20a2c7a99ca6e83b45161a937211fde43ad1
Antony Blakey <antony.blakey@gmail.com>
2022-05-23 06:51:32 +0200

    Fill out schema and generators (#38)
    * Update schema
    
    * Update sample for new schema
    
    * Incorporate ebnf generator
    
    * Remove errant underscore in name
    
    * Add generated solidity grammar.ebnf for sample

--- tools/syntax-schema/src/schema.rs
+++ tools/syntax-schema/src/schema.rs
@@ -1,10 +1,17 @@
-use serde::Deserialize;
-use std::{collections::HashMap, path::PathBuf};
+use indexmap::IndexMap;
+use semver::Version;
+use serde::{
+    de::{self, MapAccess, Visitor},
+    ser::SerializeMap,
+    Deserialize, Serialize, Serializer,
+};
+use serde_yaml::Value;
+use std::{collections::BTreeMap, fmt, path::PathBuf, rc::Rc};
 
 #[derive(Clone, Debug)]
 pub struct Grammar {
     pub manifest: Manifest,
-    pub topics: HashMap<String, Vec<Rule>>,
+    pub productions: IndexMap<String, Vec<Production>>,
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -28,111 +35,243 @@ pub struct Topic {
     pub definition: String,
 }
 
-#[derive(Deserialize, Clone, Debug)]
-pub struct Rule {
-    pub name: Option<String>,
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Production {
+    pub name: String,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
 
+    // TODO: use Either for the next two fields,
+    // but it will require custom ser/de methods.
+    // Or, use "0.0.0" and elide on serializing, which
+    // is logically consistent.
     #[serde(flatten)]
-    pub production: Production,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub expr: Option<ExpressionRef>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "BTreeMap::is_empty")]
+    pub versions: BTreeMap<Version, ExpressionRef>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
-#[serde(untagged)]
-pub enum Production {
-    Alternation(Alternation),
-    Difference(Difference),
-    Not(Not),
-    OneOrMore(OneOrMore),
-    Optional(Optional),
-    Range(Range),
-    Reference(Reference),
-    Sequence(Sequence),
-    Terminal(Terminal),
-    Versions(Versions),
-    ZeroOrMore(ZeroOrMore),
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Expression {
+    pub config: ExpressionConfig,
+    pub ebnf: EBNF,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+impl Serialize for Expression {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_map(None)?;
+        match &self.ebnf {
+            // These two are ugly - serde has no way of emitting a map entry with
+            // no value, which is valid in YAML. So these end up as e.g. `end: ~`
+            EBNF::End => state.serialize_entry("end", &Value::Null),
+            EBNF::Any => state.serialize_entry("any", &Value::Null),
+
+            EBNF::ZeroOrMore(expr) => state.serialize_entry("zeroOrMore", &expr),
+            EBNF::OneOrMore(expr) => state.serialize_entry("oneOrMore", &expr),
+            EBNF::Optional(expr) => state.serialize_entry("optional", &expr),
+            EBNF::Not(expr) => state.serialize_entry("not", &expr),
+            EBNF::Choice(exprs) => state.serialize_entry("choice", &exprs),
+            EBNF::Sequence(exprs) => state.serialize_entry("sequence", &exprs),
+            EBNF::Terminal(string) => state.serialize_entry("terminal", &string),
+            EBNF::Reference(name) => state.serialize_entry("reference", &name),
+            EBNF::Difference(difference) => state.serialize_entry("difference", &difference),
+            EBNF::Range(range) => state.serialize_entry("range", &range),
+        }?;
+        if !self.config.is_default() {
+            state.serialize_entry("config", &self.config)?;
+        };
+        state.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Expression {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(field_identifier, rename_all = "camelCase")]
+        enum Field {
+            Config,
+            End,
+            Any,
+            ZeroOrMore,
+            OneOrMore,
+            Optional,
+            Not,
+            Choice,
+            Sequence,
+            Terminal,
+            Reference,
+            Difference,
+            Range,
+        }
+
+        struct ExpressionVisitor;
+
+        impl<'de> Visitor<'de> for ExpressionVisitor {
+            type Value = Expression;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Expression")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Expression, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut config = None;
+                let mut ebnf = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Config => {
+                            if config.is_some() {
+                                return Err(de::Error::duplicate_field("config"));
+                            }
+                        }
+                        Field::End
+                        | Field::Any
+                        | Field::ZeroOrMore
+                        | Field::OneOrMore
+                        | Field::Optional
+                        | Field::Not
+                        | Field::Choice
+                        | Field::Sequence
+                        | Field::Terminal
+                        | Field::Reference
+                        | Field::Difference
+                        | Field::Range => {
+                            if ebnf.is_some() {
+                                return Err(de::Error::duplicate_field("ebnf element"));
+                            }
+                        }
+                    }
+                    match key {
+                        Field::Config => config = Some(map.next_value()?),
+                        Field::End => ebnf = Some(EBNF::End),
+                        Field::Any => ebnf = Some(EBNF::Any),
+                        Field::ZeroOrMore => ebnf = Some(EBNF::ZeroOrMore(map.next_value()?)),
+                        Field::OneOrMore => ebnf = Some(EBNF::OneOrMore(map.next_value()?)),
+                        Field::Optional => ebnf = Some(EBNF::Optional(map.next_value()?)),
+                        Field::Not => ebnf = Some(EBNF::Not(map.next_value()?)),
+                        Field::Choice => ebnf = Some(EBNF::Choice(map.next_value()?)),
+                        Field::Sequence => ebnf = Some(EBNF::Sequence(map.next_value()?)),
+                        Field::Terminal => ebnf = Some(EBNF::Terminal(map.next_value()?)),
+                        Field::Reference => ebnf = Some(EBNF::Reference(map.next_value()?)),
+                        Field::Difference => ebnf = Some(EBNF::Difference(map.next_value()?)),
+                        Field::Range => ebnf = Some(EBNF::Range(map.next_value()?)),
+                    }
+                }
+                let config = config.unwrap_or_default();
+                let ebnf = ebnf.ok_or_else(|| de::Error::missing_field("ebnf element"))?;
+                Ok(Expression { config, ebnf })
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &[
+            "config",
+            "end",
+            "any",
+            "zeroOrMore",
+            "oneOrMore",
+            "optional",
+            "not",
+            "choice",
+            "sequence",
+            "difference",
+            "terminal",
+            "reference",
+            "range",
+        ];
+        deserializer.deserialize_struct("Expression", FIELDS, ExpressionVisitor)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
-pub struct Alternation {
-    pub alternation: Vec<Rule>,
+pub struct EBNFDifference {
+    pub minuend: ExpressionRef,
+    pub subtrahend: ExpressionRef,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
-pub struct Difference {
-    pub difference: DifferenceProperties,
+pub struct EBNFRange {
+    pub from: char,
+    pub to: char,
 }
 
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct DifferenceProperties {
-    pub minuend: String,
-    pub subtrahend: String,
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum EBNF {
+    End,
+    Any,
+    ZeroOrMore(ExpressionRef),
+    OneOrMore(ExpressionRef),
+    Optional(ExpressionRef),
+    Not(ExpressionRef),
+    Choice(Vec<ExpressionRef>),
+    Sequence(Vec<ExpressionRef>),
+    Terminal(String),
+    Reference(String),
+    Difference(EBNFDifference),
+    Range(EBNFRange),
 }
 
-#[derive(Deserialize, Clone, Debug)]
+pub type ExpressionRef = Rc<Expression>;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
-pub struct Not {
-    pub not: String,
+pub struct ExpressionConfig {
+    #[serde(default)]
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub ignore: bool,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub nomap: bool,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub map: Option<String>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub unwrap: bool,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "std::ops::Not::not")]
+    pub chain: bool,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub lookahead: Option<ExpressionRef>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct OneOrMore {
-    #[serde(rename = "oneOrMore")]
-    pub one_or_more: Box<Rule>,
+impl ExpressionConfig {
+    pub fn is_default(&self) -> bool {
+        !self.ignore
+            && !self.nomap
+            && self.map.is_none()
+            && !self.unwrap
+            && !self.chain
+            && self.lookahead.is_none()
+    }
 }
 
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Optional {
-    pub optional: Box<Rule>,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Range {
-    pub range: RangeProperties,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct RangeProperties {
-    pub from: String,
-    pub to: String,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Reference {
-    pub reference: String,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Sequence {
-    pub sequence: Vec<Rule>,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Terminal {
-    pub terminal: String,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct Versions {
-    pub versions: HashMap<String, Rule>,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-#[serde(deny_unknown_fields)]
-pub struct ZeroOrMore {
-    #[serde(rename = "zeroOrMore")]
-    pub zero_or_more: Box<Rule>,
+impl Default for ExpressionConfig {
+    fn default() -> Self {
+        Self {
+            ignore: false,
+            nomap: false,
+            map: None,
+            unwrap: false,
+            chain: false,
+            lookahead: None,
+        }
+    }
 }
 
 pub fn load_grammar(manifest_path: &PathBuf) -> Grammar {
@@ -140,7 +279,7 @@ pub fn load_grammar(manifest_path: &PathBuf) -> Grammar {
     let manifest_path_str = &manifest_path.to_str().unwrap();
     let manifest: Manifest = serde_yaml::from_slice(&contents).expect(manifest_path_str);
 
-    let topics: HashMap<String, Vec<Rule>> = manifest
+    let topics: IndexMap<String, Vec<Production>> = manifest
         .sections
         .iter()
         .flat_map(|section| &section.topics)
@@ -149,11 +288,14 @@ pub fn load_grammar(manifest_path: &PathBuf) -> Grammar {
             let topic_path_str = topic_path.to_str().unwrap();
 
             let contents = std::fs::read(&topic_path).expect(topic_path_str);
-            let rules: Vec<Rule> = serde_yaml::from_slice(&contents).expect(topic_path_str);
+            let rules: Vec<Production> = serde_yaml::from_slice(&contents).expect(topic_path_str);
 
             return (topic.definition.clone(), rules);
         })
         .collect();
 
-    return Grammar { manifest, topics };
+    return Grammar {
+        manifest,
+        productions: topics,
+    };
 }
--- tools/syntax-schema/src/validation.rs
+++ tools/syntax-schema/src/validation.rs
@@ -10,10 +10,6 @@ pub fn validate(_grammar: &Grammar) {
     // 1) Making sure there are no orphaned nodes by mistake.
     // 2) Making sure all referenced nodes actually exist in the grammar.
     // 3) Versions specified map to supported language versions.
-    // 4) Ranges have valid from/to values, and map to valid Solidity characters.
-    // 5) Make sure top-level rules have a name, so that we can refer to them from other rules.
-    // 6) "Not" has a valid/supported character
-    // 7) version breaks are constrained to top level nodes only
 
     // .... Any other domain-specific rules we want to add later.
 }
