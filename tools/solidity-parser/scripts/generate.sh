16f265ebba6fa3a3fe8687e59a2560f5c8d317e1
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2022-06-06 05:03:13 -0700

    fast scripts - no cargo install (#58)

--- tools/solidity-parser/scripts/generate.sh
+++ tools/solidity-parser/scripts/generate.sh
@@ -7,13 +7,11 @@ PROJECT_DIR=$(dirname "$THIS_DIR")
 # shellcheck source=/dev/null
 [[ "${HERMIT_ENV:-}" == "$PROJECT_DIR" ]] || source "$PROJECT_DIR/bin/activate-hermit"
 
-cargo install --path ../syntax-schema
-
 ###################################################
 # Solidity from the original antlr grammar
 ###################################################
 
-manifest_to_chumsky \
+cargo run --manifest-path "../syntax-schema/Cargo.toml" --bin "manifest_to_chumsky" -- \
   --manifest-input "$PROJECT_DIR/syntax/solidity/original/manifest.yml" \
   --no-default-map --box-non-tokens \
   --chumsky-output "$PROJECT_DIR/src/parser.rs"
