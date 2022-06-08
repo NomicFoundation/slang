f64bbbf8a0bd98d9e192f747402d15d85104ca7c
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2022-06-08 07:17:24 -0700

    split mkdocs config for spec generation (#65)

--- documentation/scripts/watch.sh
+++ documentation/scripts/watch.sh
@@ -8,13 +8,13 @@ PROJECT_DIR=$(dirname "$THIS_DIR")
 [[ "${HERMIT_ENV:-}" == "$PROJECT_DIR" ]] || source "$PROJECT_DIR/bin/activate-hermit"
 
 (
-  # Run setup first
-  "$THIS_DIR/setup.sh"
+  # Perform a full build first
+  "$THIS_DIR/build.sh"
 )
 
 (
   printf "\n\n🌐 Serving Documentation 🌐\n\n\n"
-  cd "$PROJECT_DIR"
+  cd "$PROJECT_DIR/target"
   # VS Code devcontainer is configured to automatically render the website when this port is forwarded.
   python3 -m pipenv run mkdocs serve --watch-theme --dev-addr "127.0.0.1:5353"
 )
--- tools/syntax-schema/src/spec/mod.rs
+++ tools/syntax-schema/src/spec/mod.rs
@@ -1,2 +1,3 @@
 pub mod generator;
 pub mod grammar_spec;
+pub mod spec_navigation;
