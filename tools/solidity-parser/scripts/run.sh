20fef64d5c8364851d4d1621a9caad3316567340
AntonyBlakey <antony.blakey@gmail.com>
2022-06-10 08:43:48 +0200

    Satisfy linter

--- tools/solidity-parser/scripts/run.sh
+++ tools/solidity-parser/scripts/run.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 PROJECT_DIR=$(dirname "$THIS_DIR")
-REPO_ROOT=$(realpath "$PROJECT_DIR/../..")
 
 # shellcheck source=/dev/null
 [[ "${HERMIT_ENV:-}" == "$PROJECT_DIR" ]] || source "$PROJECT_DIR/bin/activate-hermit"
