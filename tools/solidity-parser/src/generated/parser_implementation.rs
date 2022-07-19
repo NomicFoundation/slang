NomicFoundation/slang

commit 3439de5a176e8c9b2dd823252d167cb25abaf99f

Flatten/fold expression tree (#92)

tools/solidity-parser/src/generated/derived.ebnf
@@ -6,19 +6,25 @@
  * First Section: Trivia
  *)
 
-«IGNORE» = { «Whitespace» | «Comment» | «LineComment» } ;
+«LeadingTrivia» = { «Whitespace» | «EndOfLine» | «Comment» | «LineComment» } ;
 
-«Whitespace» = 1…*{ '\u{20}' | '\u{9}' | '\u{d}' | '\u{a}' } ;
+«TrailingTrivia» = [ { «Whitespace» | «Comment» } ( «EndOfLine» | «LineComment» ) ] ;
+
+«EndOfFileTrivia» = { «Whitespace» | «Comment» | «LineComment» } ;
+
+«Whitespace» = 1…*{ '\u{20}' | '\u{9}' } ;
+
+«EndOfLine» = 1…*{ '\u{d}' | '\u{a}' } ;
 
 «Comment» = '/*' { ¬'*' | 1…*{ '*' } ¬( '*' | '/' ) } { '*' } '*/' ;
 
-«LineComment» = '//' { ¬( '\u{a}' | '\u{d}' ) } ;
+«LineComment» = '//' { ¬( '\u{d}' | '\u{a}' ) } ;
 
 (*
  * First Section: Solidity File
  *)
 
-SourceUnit = «IGNORE» { Directive | Definition } $ ;
+SourceUnit = «LeadingTrivia» { Directive | Definition } «EndOfFileTrivia» $ ;
 
 Directive = «PragmaDirective» | ImportDirective | UsingDirective ;
 
@@ -132,61 +138,45 @@ MappingType = 'mapping' '(' ( ElementaryTypeWithoutPayable | IdentifierPath ) '=
 
 DataLocation = 'memory' | 'storage' | 'calldata' ;
 
-Expression = AssignmentExpression ;
+Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | FunctionCallOptionsExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
 
-AssignmentExpression = ConditionalExpression { AssignmentOperator Expression } ;
+AssignmentExpression = Expression ( '=' | '|=' | '^=' | '&=' | '<<=' | '>>=' | '>>>=' | '+=' | '-=' | '*=' | '/=' | '%=' ) Expression ;
 
-AssignmentOperator = '=' | '|=' | '^=' | '&=' | '<<=' | '>>=' | '>>>=' | '+=' | '-=' | '*=' | '/=' | '%=' ;
+ConditionalExpression = Expression '?' Expression ':' Expression ;
 
-ConditionalExpression = OrExpression [ '?' Expression ':' Expression ] ;
+OrExpression = Expression '||' Expression ;
 
-OrExpression = AndExpression { '||' AndExpression } ;
+AndExpression = Expression '&&' Expression ;
 
-AndExpression = EqualityComparisonExpression { '&&' EqualityComparisonExpression } ;
+EqualityComparisonExpression = Expression ( '==' | '!=' ) Expression ;
 
-EqualityComparisonExpression = OrderComparisonExpression { EqualityComparisonOperator OrderComparisonExpression } ;
+OrderComparisonExpression = Expression ( '<' | '>' | '<=' | '>=' ) Expression ;
 
-EqualityComparisonOperator = '==' | '!=' ;
+BitOrExpression = Expression '|' Expression ;
 
-OrderComparisonExpression = BitOrExpression { OrderComparisonOperator BitOrExpression } ;
+BitXOrExpression = Expression '^' Expression ;
 
-OrderComparisonOperator = '<' | '>' | '<=' | '>=' ;
+BitAndExpression = Expression '&' Expression ;
 
-BitOrExpression = BitXOrExpression { '|' BitXOrExpression } ;
+ShiftExpression = Expression ( '<<' | '>>' | '>>>' ) Expression ;
 
-BitXOrExpression = BitAndExpression { '^' BitAndExpression } ;
+AddSubExpression = Expression ( '+' | '-' ) Expression ;
 
-BitAndExpression = ShiftExpression { '&' ShiftExpression } ;
+MulDivModExpression = Expression ( '*' | '/' | '%' ) Expression ;
 
-ShiftExpression = AddSubExpression { ShiftOperator AddSubExpression } ;
+ExponentiationExpression = Expression '**' Expression ;
 
-ShiftOperator = '<<' | '>>' | '>>>' ;
+UnarySuffixExpression = Expression ( '++' | '--' ) ;
 
-AddSubExpression = MulDivModExpression { AddSubOperator MulDivModExpression } ;
+UnaryPrefixExpression = ( '++' | '--' | '!' | '~' | 'delete' | '-' ) Expression ;
 
-AddSubOperator = '+' | '-' ;
+FunctionCallExpression = Expression ArgumentList ;
 
-MulDivModExpression = ExpExpression { MulDivModOperator ExpExpression } ;
+FunctionCallOptionsExpression = Expression '{' 1…*{ NamedArgument / ',' } '}' ;
 
-MulDivModOperator = '*' | '/' | '%' ;
+MemberAccessExpression = Expression '.' ( «Identifier» | 'address' ) ;
 
-ExpExpression = UnarySuffixExpression { '**' Expression } ;
-
-UnarySuffixExpression = UnaryPrefixExpression [ UnarySuffixOperator ] ;
-
-UnarySuffixOperator = '++' | '--' ;
-
-UnaryPrefixExpression = [ UnaryPrefixOperator ] FunctionCallExpression ;
-
-UnaryPrefixOperator = '++' | '--' | '!' | '~' | 'delete' | '-' ;
-
-FunctionCallExpression = FunctionCallOptionsExpression { ArgumentList } ;
-
-FunctionCallOptionsExpression = MemberAccessExpression { '{' 1…*{ NamedArgument / ',' } '}' } ;
-
-MemberAccessExpression = IndexAccessExpression { '.' ( «Identifier» | 'address' ) } ;
-
-IndexAccessExpression = PrimaryExpression { '[' [ Expression ] [ ':' [ Expression ] ] ']' } ;
+IndexAccessExpression = Expression '[' [ Expression ] [ ':' [ Expression ] ] ']' ;
 
 PrimaryExpression = 'payable' ArgumentList | 'type' '(' TypeName ')' | 'new' TypeName | '(' 1…*{ [ Expression ] / ',' } ')' | '[' 1…*{ Expression / ',' } ']' | «Identifier» | Literal | ElementaryTypeWithoutPayable ;
 
@@ -214,7 +204,7 @@ ContinueStatement = 'continue' ';' ;
 
 BreakStatement = 'break' ';' ;
 
-TryStatement = 'try' Expression [ 'returns' NonEmptyParameterList ] Block CatchClause { CatchClause } ;
+TryStatement = 'try' Expression [ 'returns' NonEmptyParameterList ] Block 1…*{ CatchClause } ;
 
 CatchClause = 'catch' [ [ «Identifier» ] NonEmptyParameterList ] Block ;
 
@@ -342,4 +332,4 @@ YulLiteral = «YulDecimalNumberLiteral» | «YulHexLiteral» | «AsciiStringLite
 
 «YulDecimalNumberLiteral» = '0' | '1'…'9' { '0'…'9' } ;
 
-«YulHexLiteral» = '0x' ( '0'…'9' | 'a'…'f' | 'A'…'F' ) { '0'…'9' | 'a'…'f' | 'A'…'F' } ;
+«YulHexLiteral» = '0x' 1…*{ '0'…'9' | 'a'…'f' | 'A'…'F' } ;

commit 376616ed8033a8afb4dea290d40a6076bafecb39

fix FunctionCallOptions as it is allowed as an expression
Broken in #92 as it was absorbed as a top-level expression, while it is part of FunctionCallExpression

tools/solidity-parser/src/generated/derived.ebnf
@@ -132,7 +132,7 @@ MappingType = 'mapping' '(' ( ElementaryType | IdentifierPath ) '=>' TypeName ')
 
 DataLocation = 'memory' | 'storage' | 'calldata' ;
 
-Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | FunctionCallOptionsExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
+Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
 
 AssignmentExpression = Expression ( '=' | '|=' | '^=' | '&=' | '<<=' | '>>=' | '>>>=' | '+=' | '-=' | '*=' | '/=' | '%=' ) Expression ;
 
@@ -164,9 +164,7 @@ UnarySuffixExpression = Expression ( '++' | '--' ) ;
 
 UnaryPrefixExpression = ( '++' | '--' | '!' | '~' | '-' ) Expression ;
 
-FunctionCallExpression = Expression ArgumentList ;
-
-FunctionCallOptionsExpression = Expression '{' 1…*{ NamedArgument / ',' } '}' ;
+FunctionCallExpression = Expression [ '{' 1…*{ NamedArgument / ',' } '}' ] ArgumentList ;
 
 MemberAccessExpression = Expression '.' ( «Identifier» | 'address' ) ;
 

tools/solidity-parser/src/generated/parser_implementation.rs
@@ -4398,13 +4398,13 @@ impl Parsers {
             })
             .boxed();
 
-        // FunctionCallOptionsExpression = Expression '{' 1…*{ NamedArgument / ',' } '}' ;
-        let function_call_options_expression_parser = member_access_expression_parser
+        // FunctionCallExpression = Expression [ '{' 1…*{ NamedArgument / ',' } '}' ] ArgumentList ;
+        let function_call_expression_parser = member_access_expression_parser
             .clone()
             .then(
                 leading_trivia_parser
                     .clone()
-                    .then(just('{').map(|_| FixedSizeTerminal::<1>()))
+                    .then(just("{").map(|_| FixedSizeTerminal::<1usize>()))
                     .then(trailing_trivia_parser.clone())
                     .map(
                         |((leading, content), trailing)| FixedSizeTerminalWithTrivia {
@@ -4432,17 +4432,15 @@ impl Parsers {
                                     .repeated(),
                             )
                             .map(repetition_mapper)
-                            .map(
-                                |(elements, separators)| function_call_options_expression::_T1 {
-                                    elements,
-                                    separators,
-                                },
-                            ),
+                            .map(|(elements, separators)| function_call_expression::_T2 {
+                                elements,
+                                separators,
+                            }),
                     )
                     .then(
                         leading_trivia_parser
                             .clone()
-                            .then(just('}').map(|_| FixedSizeTerminal::<1>()))
+                            .then(just("}").map(|_| FixedSizeTerminal::<1usize>()))
                             .then(trailing_trivia_parser.clone())
                             .map(
                                 |((leading, content), trailing)| FixedSizeTerminalWithTrivia {
@@ -4452,31 +4450,12 @@ impl Parsers {
                                 },
                             ),
                     )
-                    .map(|v| function_call_options_expression::Operator::from_parse(v))
+                    .map(|v| function_call_expression::_T1::from_parse(v))
+                    .or_not()
+                    .then(argument_list_parser.clone())
+                    .map(|v| function_call_expression::Operator::from_parse(v))
                     .repeated(),
             )
-            .map(|(operand, operators)| {
-                if operators.is_empty() {
-                    operand
-                } else {
-                    operators
-                        .into_iter()
-                        .fold(operand, |left_operand, operator| {
-                            Box::new(expression::Expression::FunctionCallOptionsExpression(
-                                function_call_options_expression::E {
-                                    left_operand,
-                                    operator,
-                                },
-                            ))
-                        })
-                }
-            })
-            .boxed();
-
-        // FunctionCallExpression = Expression ArgumentList ;
-        let function_call_expression_parser = function_call_options_expression_parser
-            .clone()
-            .then(argument_list_parser.clone().repeated())
             .map(|(operand, operators)| {
                 if operators.is_empty() {
                     operand
@@ -5139,7 +5118,7 @@ impl Parsers {
             })
             .boxed();
 
-        // Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | FunctionCallOptionsExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
+        // Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
         expression_parser.define(assignment_expression_parser.clone().boxed());
 
         // ConstantDefinition = TypeName 'constant' «Identifier» '=' Expression ';' ;
@@ -6576,7 +6555,6 @@ impl Parsers {
             struct_definition: struct_definition_parser,
             index_access_expression: index_access_expression_parser,
             member_access_expression: member_access_expression_parser,
-            function_call_options_expression: function_call_options_expression_parser,
             function_call_expression: function_call_expression_parser,
             unary_prefix_expression: unary_prefix_expression_parser,
             unary_suffix_expression: unary_suffix_expression_parser,

tools/solidity-parser/src/generated/parser_interface.rs
@@ -112,7 +112,6 @@ pub struct Parsers {
     pub struct_definition: ParserType<StructDefinition>,
     pub index_access_expression: ParserType<IndexAccessExpression>,
     pub member_access_expression: ParserType<MemberAccessExpression>,
-    pub function_call_options_expression: ParserType<FunctionCallOptionsExpression>,
     pub function_call_expression: ParserType<FunctionCallExpression>,
     pub unary_prefix_expression: ParserType<UnaryPrefixExpression>,
     pub unary_suffix_expression: ParserType<UnarySuffixExpression>,

tools/solidity-parser/src/generated/tree_implementation.rs
@@ -3077,7 +3077,7 @@ impl member_access_expression::Operator {
     }
 }
 
-impl Default for function_call_options_expression::_T1 {
+impl Default for function_call_expression::_T2 {
     fn default() -> Self {
         Self {
             elements: Default::default(),
@@ -3085,42 +3085,62 @@ impl Default for function_call_options_expression::_T1 {
         }
     }
 }
-impl DefaultTest for function_call_options_expression::_T1 {
+impl DefaultTest for function_call_expression::_T2 {
     fn is_default(&self) -> bool {
         self.elements.is_default() && self.separators.is_default()
     }
 }
-impl function_call_options_expression::Operator {
+impl Default for function_call_expression::_T1 {
+    fn default() -> Self {
+        Self {
+            open_brace: Default::default(),
+            named_arguments: Default::default(),
+            close_brace: Default::default(),
+        }
+    }
+}
+impl DefaultTest for function_call_expression::_T1 {
+    fn is_default(&self) -> bool {
+        self.open_brace.is_default()
+            && self.named_arguments.is_default()
+            && self.close_brace.is_default()
+    }
+}
+impl function_call_expression::_T1 {
     pub fn from_parse(
-        ((open_brace_char, named_arguments), close_brace_char): (
+        ((open_brace, named_arguments), close_brace): (
             (
-                FixedSizeTerminalWithTrivia<1>,
-                function_call_options_expression::_T1,
+                FixedSizeTerminalWithTrivia<1usize>,
+                function_call_expression::_T2,
             ),
-            FixedSizeTerminalWithTrivia<1>,
+            FixedSizeTerminalWithTrivia<1usize>,
         ),
     ) -> Self {
         Self {
-            open_brace_char,
+            open_brace,
             named_arguments,
-            close_brace_char,
+            close_brace,
         }
     }
 }
-impl Default for function_call_options_expression::Operator {
+impl function_call_expression::Operator {
+    pub fn from_parse(
+        (_t1, argument_list): (Option<function_call_expression::_T1>, ArgumentList),
+    ) -> Self {
+        Self { _t1, argument_list }
+    }
+}
+impl Default for function_call_expression::Operator {
     fn default() -> Self {
         Self {
-            open_brace_char: Default::default(),
-            named_arguments: Default::default(),
-            close_brace_char: Default::default(),
+            _t1: Default::default(),
+            argument_list: Default::default(),
         }
     }
 }
-impl DefaultTest for function_call_options_expression::Operator {
+impl DefaultTest for function_call_expression::Operator {
     fn is_default(&self) -> bool {
-        self.open_brace_char.is_default()
-            && self.named_arguments.is_default()
-            && self.close_brace_char.is_default()
+        self._t1.is_default() && self.argument_list.is_default()
     }
 }
 

tools/solidity-parser/src/generated/tree_interface.rs
@@ -2376,45 +2376,39 @@ pub mod member_access_expression {
     }
 }
 
-/// FunctionCallOptionsExpression = Expression '{' 1…*{ NamedArgument / ',' } '}' ;
-pub type FunctionCallOptionsExpression = Expression;
-pub mod function_call_options_expression {
+/// FunctionCallExpression = Expression [ '{' 1…*{ NamedArgument / ',' } '}' ] ArgumentList ;
+pub type FunctionCallExpression = Expression;
+pub mod function_call_expression {
     #[allow(unused_imports)]
     use super::*;
     #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
-    pub struct _T1 {
+    pub struct _T2 {
         #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
         pub elements: Vec<NamedArgument>,
         #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
         pub separators: Vec<FixedSizeTerminalWithTrivia<1>>,
     }
     #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct _T1 {
+        #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
+        pub open_brace: FixedSizeTerminalWithTrivia<1usize>,
+        #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
+        pub named_arguments: function_call_expression::_T2,
+        #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
+        pub close_brace: FixedSizeTerminalWithTrivia<1usize>,
+    }
+    #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
     pub struct Operator {
         #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
-        pub open_brace_char: FixedSizeTerminalWithTrivia<1>,
+        pub _t1: Option<function_call_expression::_T1>,
         #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
-        pub named_arguments: function_call_options_expression::_T1,
-        #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
-        pub close_brace_char: FixedSizeTerminalWithTrivia<1>,
+        pub argument_list: ArgumentList,
     }
     #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
     pub struct E {
         pub left_operand: Expression,
         #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
-        pub operator: function_call_options_expression::Operator,
-    }
-}
-
-/// FunctionCallExpression = Expression ArgumentList ;
-pub type FunctionCallExpression = Expression;
-pub mod function_call_expression {
-    #[allow(unused_imports)]
-    use super::*;
-    #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
-    pub struct E {
-        pub left_operand: Expression,
-        #[serde(default, skip_serializing_if = "DefaultTest::is_default")]
-        pub operator: ArgumentList,
+        pub operator: function_call_expression::Operator,
     }
 }
 
@@ -2633,7 +2627,7 @@ pub mod assignment_expression {
     }
 }
 
-/// Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | FunctionCallOptionsExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
+/// Expression = AssignmentExpression | ConditionalExpression | OrExpression | AndExpression | EqualityComparisonExpression | OrderComparisonExpression | BitOrExpression | BitXOrExpression | BitAndExpression | ShiftExpression | AddSubExpression | MulDivModExpression | ExponentiationExpression | UnarySuffixExpression | UnaryPrefixExpression | FunctionCallExpression | MemberAccessExpression | IndexAccessExpression | PrimaryExpression ;
 pub type Expression = Box<expression::Expression>;
 pub mod expression {
     #[allow(unused_imports)]
@@ -2656,7 +2650,6 @@ pub mod expression {
         UnarySuffixExpression(unary_suffix_expression::E),
         UnaryPrefixExpression(unary_prefix_expression::E),
         FunctionCallExpression(function_call_expression::E),
-        FunctionCallOptionsExpression(function_call_options_expression::E),
         MemberAccessExpression(member_access_expression::E),
         IndexAccessExpression(index_access_expression::E),
         PrimaryExpression(primary_expression::E),

tools/solidity-parser/syntax/solidity/temporary-split/grammar.yml
@@ -548,7 +548,6 @@
     - reference: UnarySuffixExpression
     - reference: UnaryPrefixExpression
     - reference: FunctionCallExpression
-    - reference: FunctionCallOptionsExpression
     - reference: MemberAccessExpression
     - reference: IndexAccessExpression
     - reference: PrimaryExpression
@@ -676,16 +675,15 @@
 - name: FunctionCallExpression
   sequence:
     - reference: Expression
+    - optional:
+        delimitedBy:
+          open: "{"
+          oneOrMore:
+            reference: NamedArgument
+            separator:
+              terminal: ","
+          close: "}"
     - reference: ArgumentList
-- name: FunctionCallOptionsExpression
-  sequence:
-    - reference: Expression
-    - terminal: "{"
-    - oneOrMore:
-        reference: NamedArgument
-        separator:
-          terminal: ","
-    - terminal: "}"
 - name: MemberAccessExpression
   sequence:
     - reference: Expression

tools/solidity-parser/syntax/solidity/temporary-split/splits.yml
@@ -86,7 +86,6 @@
           - UnarySuffixExpression
           - UnaryPrefixExpression
           - FunctionCallExpression
-          - FunctionCallOptionsExpression
           - MemberAccessExpression
           - IndexAccessExpression
           - PrimaryExpression

tools/solidity-parser/syntax/solidity/topics/02-todo-section/01-todo-topic.yml
@@ -437,7 +437,6 @@
     - reference: UnarySuffixExpression
     - reference: UnaryPrefixExpression
     - reference: FunctionCallExpression
-    - reference: FunctionCallOptionsExpression
     - reference: MemberAccessExpression
     - reference: IndexAccessExpression
     - reference: PrimaryExpression
@@ -581,17 +580,15 @@
 - name: FunctionCallExpression
   sequence:
     - reference: Expression
+    - optional:
+        delimitedBy:
+          open: "{"
+          oneOrMore:
+            reference: NamedArgument
+            separator:
+              terminal: ","
+          close: "}"
     - reference: ArgumentList
-
-- name: FunctionCallOptionsExpression
-  sequence:
-    - reference: Expression
-    - terminal: "{"
-    - oneOrMore:
-        reference: NamedArgument
-        separator:
-          terminal: ","
-    - terminal: "}"
 
 - name: MemberAccessExpression
   sequence: