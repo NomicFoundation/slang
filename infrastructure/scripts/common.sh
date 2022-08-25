0a27313e34706f87317fb490fce8f0a1fa2e9847
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2022-08-25 11:32:43 -0700

    create infrastructure top-level dir

--- infrastructure/scripts/common.sh
+++ infrastructure/scripts/common.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 # shellcheck source=/dev/null
-[[ -z "${HERMIT_ENV:-}" ]] && source "$THIS_DIR/../../../bin/activate-hermit"
+[[ -z "${HERMIT_ENV:-}" ]] && source "$THIS_DIR/../../bin/activate-hermit"
 
 export LINTING_DIR="$THIS_DIR/.."
 export NPM_BIN_DIR="$LINTING_DIR/node_modules/.bin"
