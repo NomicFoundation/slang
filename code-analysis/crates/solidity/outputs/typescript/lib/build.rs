df4020f360e09758730ad9e1bf1f96b505ceb807
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2022-11-21 19:15:08 +0200

    split lib and cli crates in codegen outputs

--- code-analysis/crates/solidity/outputs/typescript/lib/build.rs
+++ code-analysis/crates/solidity/outputs/typescript/lib/build.rs
@@ -1,3 +1,5 @@
+extern crate napi_build;
+
 use codegen_parser::GrammarParserGeneratorExtensions;
 use codegen_schema::Grammar;
 use codegen_utils::context::CodegenContext;
@@ -5,15 +7,17 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
-    let repo_dir = crate_dir.join("../../../../..").canonicalize()?;
+    let repo_dir = crate_dir.join("../../../../../..").canonicalize()?;
     let grammar_file = repo_dir.join("code-analysis/crates/solidity/inputs/schema/manifest.yml");
     let output_dir = crate_dir.join("src/generated");
 
     CodegenContext::with_context(repo_dir.clone(), |codegen| {
         let grammar = Grammar::from_manifest(codegen, &grammar_file);
-        grammar.generate_rust_lib_sources(codegen, &output_dir);
+        grammar.generate_typescript_lib_sources(codegen, &output_dir);
         return Ok(());
     })?;
+
+    napi_build::setup();
 
     return Ok(());
 }
