67da8152341e01a2e05e3ba9332e5bb96aa3d969
Antony Blakey <antony.blakey@gmail.com>
2022-11-22 18:54:52 +1100

    DRY GrammarParserGeneratorExtensions (#206)

--- code-analysis/crates/codegen/parser/src/chumsky/grammar.rs
+++ code-analysis/crates/codegen/parser/src/chumsky/grammar.rs
@@ -10,86 +10,42 @@ use super::combinator_tree::CombinatorTree;
 pub trait GrammarParserGeneratorExtensions {
     fn generate_rust_lib_sources(
         &self,
-        codegen: &mut CodegenContext,
+        context: &mut CodegenContext,
         output_dir: &std::path::PathBuf,
     );
     fn generate_typescript_lib_sources(
         &self,
-        codegen: &mut CodegenContext,
+        context: &mut CodegenContext,
         output_dir: &std::path::PathBuf,
     );
 }
 
 trait PrivateGrammarParserGeneratorExtensions {
-    fn generate_typescript_lib_sources(
-        &self,
-        codegen: &mut CodegenContext,
-        output_dir: &std::path::PathBuf,
-    );
+    fn create_code_generator(&self) -> CodeGenerator;
 }
 
 impl GrammarParserGeneratorExtensions for Grammar {
     fn generate_rust_lib_sources(
         &self,
-        codegen: &mut CodegenContext,
+        context: &mut CodegenContext,
         output_dir: &std::path::PathBuf,
     ) {
-        let mut version_breaks = BTreeSet::new();
-        for production in self.productions.values().flatten() {
-            for version in production.versions.keys() {
-                version_breaks.insert(version.clone());
-            }
-        }
-
-        let mut generated_code = CodeGenerator::default();
-
-        for version in version_breaks {
-            let context = CombinatorContext::new(self, version.clone());
-
-            {
-                // Scoped to isolate the borrow
-                let mut trees_by_name = context.trees_by_name.borrow_mut();
-
-                for production in self.productions.values().flatten() {
-                    trees_by_name.insert(
-                        production.name.clone(),
-                        CombinatorTree::new(&context, &production),
-                    );
-                }
-            }
-
-            {
-                // Scoped to isolate the borrow
-                let trees_by_name = context.trees_by_name.borrow();
-
-                // Do this after all trees are created, but before any are
-                // generated, so that e.g. precedence rules can transform their
-                // members
-                for tree in trees_by_name.values() {
-                    tree.ensure_tree_is_built();
-                }
-
-                for tree in trees_by_name.values() {
-                    tree.add_to_generated_code(&mut generated_code);
-                }
-            }
-        }
-
-        if generated_code.has_errors() {
-            eprintln!("Errors:");
-            for error in generated_code.get_errors() {
-                eprintln!("  {}", error);
-            }
-        }
-
-        generated_code.write_rust_lib_sources(codegen, output_dir);
+        self.create_code_generator()
+            .write_rust_lib_sources(context, output_dir);
     }
 
     fn generate_typescript_lib_sources(
         &self,
-        codegen: &mut CodegenContext,
+        context: &mut CodegenContext,
         output_dir: &std::path::PathBuf,
     ) {
+        self.create_code_generator()
+            .write_typescript_lib_sources(context, output_dir);
+    }
+}
+
+impl PrivateGrammarParserGeneratorExtensions for Grammar {
+    fn create_code_generator(&self) -> CodeGenerator {
         let mut version_breaks = BTreeSet::new();
         for production in self.productions.values().flatten() {
             for version in production.versions.keys() {
@@ -138,6 +94,6 @@ impl GrammarParserGeneratorExtensions for Grammar {
             }
         }
 
-        generated_code.write_typescript_lib_sources(codegen, output_dir);
+        generated_code
     }
 }
