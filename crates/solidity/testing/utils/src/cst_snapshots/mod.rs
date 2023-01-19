548bc0e117ea3d0b3ee8f9f8aa6b7cb6e2f7cd91
OmarTawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-01-18 17:36:54 -0800

    make test snapshots collapsible
    To help debugging nested children

--- crates/solidity/testing/utils/src/cst_snapshots/mod.rs
+++ crates/solidity/testing/utils/src/cst_snapshots/mod.rs
@@ -1,10 +1,11 @@
-mod nodes;
-mod tree_view;
+mod test_nodes;
 
-use std::{self, fmt::Write};
+use std::{self, cmp::max, fmt::Write};
 
 use anyhow::Result;
 use solidity_rust_lib::generated::language::ParserOutput;
+
+use crate::cst_snapshots::test_nodes::TestNode;
 
 pub trait ParserOutputTestSnapshotExtensions {
     fn to_test_snapshot(&self, source_id: &str, source: &str) -> Result<String>;
@@ -12,28 +13,120 @@ pub trait ParserOutputTestSnapshotExtensions {
 
 impl ParserOutputTestSnapshotExtensions for ParserOutput {
     fn to_test_snapshot(&self, source_id: &str, source: &str) -> Result<String> {
-        let mut snapshot = String::new();
+        let mut w = String::new();
 
-        if self.error_count() == 0 {
-            writeln!(&mut snapshot, "Errors: 0")?;
-            writeln!(&mut snapshot)?;
-        } else {
-            for report in self.errors_as_strings(source_id, source, /* with_colour */ false) {
-                writeln!(&mut snapshot, "{report}")?;
-                writeln!(&mut snapshot)?;
-            }
-        }
+        write_source(&mut w, source)?;
+        writeln!(&mut w)?;
 
-        if let Some(parse_tree) = self.parse_tree() {
-            writeln!(
-                &mut snapshot,
-                "{}",
-                nodes::TestNode::from_cst(&parse_tree).to_tree_view(source)?
-            )?;
-        } else {
-            writeln!(&mut snapshot, "Tree: null")?;
-        }
+        write_errors(&mut w, &self, source_id, source)?;
+        writeln!(&mut w)?;
 
-        return Ok(snapshot);
+        write_tree(&mut w, &self, source)?;
+
+        return Ok(w);
     }
+}
+
+fn write_source<W: Write>(w: &mut W, source: &str) -> Result<()> {
+    if source.is_empty() {
+        writeln!(w, "Source: \"\"")?;
+        return Ok(());
+    }
+
+    let line_data = source
+        .lines()
+        .enumerate()
+        .map(|(index, line)| (index, line, line.chars().count()))
+        .collect::<Vec<_>>();
+
+    let source_width = {
+        let source_width = line_data
+            .iter()
+            .map(|(_, _, length)| *length)
+            .max()
+            .unwrap_or(0);
+
+        max(source_width, 80)
+    };
+
+    writeln!(w, "Source: >")?;
+
+    let mut offset = 0;
+    for (index, line, length) in line_data.iter() {
+        let range = offset..(offset + length);
+        writeln!(
+            w,
+            "  {line_number: <2} │ {line}{padding} │ {range:?}",
+            line_number = index + 1,
+            padding = " ".repeat(source_width - length),
+        )?;
+
+        offset = range.end + 1;
+    }
+
+    return Ok(());
+}
+
+fn write_errors<W: Write>(
+    w: &mut W,
+    output: &ParserOutput,
+    source_id: &str,
+    source: &str,
+) -> Result<()> {
+    let errors = output.errors_as_strings(source_id, source, /* with_colour */ false);
+
+    if errors.len() == 0 {
+        writeln!(w, "Errors: []")?;
+        return Ok(());
+    }
+
+    writeln!(w, "Errors: # {} total", errors.len())?;
+
+    for error in errors {
+        writeln!(w, "  - >")?;
+        for line in error.lines() {
+            writeln!(w, "    {line}")?;
+        }
+    }
+
+    return Ok(());
+}
+
+fn write_tree<W: Write>(w: &mut W, output: &ParserOutput, source: &str) -> Result<()> {
+    let root_node = if let Some(parse_tree) = output.parse_tree() {
+        TestNode::from_cst(&parse_tree)
+    } else {
+        writeln!(w, "Tree: null")?;
+        return Ok(());
+    };
+
+    writeln!(w, "Tree:")?;
+    write_node(w, &root_node, source, 0)?;
+    return Ok(());
+}
+
+fn write_node<W: Write>(
+    w: &mut W,
+    node: &TestNode,
+    source: &str,
+    indentation: usize,
+) -> Result<()> {
+    writeln!(
+        w,
+        "{indentation}  - {kind}: {contents} # {range}",
+        indentation = " ".repeat(4 * indentation),
+        kind = node.kind,
+        contents = node.render_contents(source)?,
+        range = if let Some(range) = &node.range {
+            format!("{range:?}")
+        } else {
+            "<empty>".to_owned()
+        },
+    )?;
+
+    for child in &node.children {
+        write_node(w, child, source, indentation + 1)?;
+    }
+
+    return Ok(());
 }
