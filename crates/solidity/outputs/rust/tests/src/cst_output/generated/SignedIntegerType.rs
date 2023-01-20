d98248f439efb1f94515e10ee8909c31c8efafd9
OmarTawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-01-20 01:52:46 -0800

    add tests for elementary types

---
+++ crates/solidity/outputs/rust/tests/src/cst_output/generated/SignedIntegerType.rs
@@ -0,0 +1,19 @@
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
+use crate::cst_output::runner::run;
+use anyhow::Result;
+
+#[test]
+fn correct_length() -> Result<()> {
+    return run("SignedIntegerType", "correct_length");
+}
+
+#[test]
+fn incorrect_length() -> Result<()> {
+    return run("SignedIntegerType", "incorrect_length");
+}
+
+#[test]
+fn no_length() -> Result<()> {
+    return run("SignedIntegerType", "no_length");
+}
