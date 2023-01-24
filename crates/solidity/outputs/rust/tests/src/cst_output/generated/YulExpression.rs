df7634b066d9e6a79bf9d4a6aca0346987e3bb76
OmarTawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-01-23 20:55:55 -0800

    fix parsing yul expressions

---
+++ crates/solidity/outputs/rust/tests/src/cst_output/generated/YulExpression.rs
@@ -0,0 +1,24 @@
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
+use crate::cst_output::runner::run;
+use anyhow::Result;
+
+#[test]
+fn decimal_literal() -> Result<()> {
+    return run("YulExpression", "decimal_literal");
+}
+
+#[test]
+fn function_call() -> Result<()> {
+    return run("YulExpression", "function_call");
+}
+
+#[test]
+fn hex_literal() -> Result<()> {
+    return run("YulExpression", "hex_literal");
+}
+
+#[test]
+fn identifier_path() -> Result<()> {
+    return run("YulExpression", "identifier_path");
+}
