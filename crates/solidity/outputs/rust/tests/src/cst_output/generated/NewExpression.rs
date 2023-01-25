35c0266c225cb03d9341dd6b806bf4bb8a5a1519
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-01-25 06:29:41 -0800

    fix NewExpression grammar (#309)

---
+++ crates/solidity/outputs/rust/tests/src/cst_output/generated/NewExpression.rs
@@ -0,0 +1,19 @@
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
+use crate::cst_output::runner::run;
+use anyhow::Result;
+
+#[test]
+fn array_1d() -> Result<()> {
+    return run("NewExpression", "array_1d");
+}
+
+#[test]
+fn array_2d() -> Result<()> {
+    return run("NewExpression", "array_2d");
+}
+
+#[test]
+fn identifier_path() -> Result<()> {
+    return run("NewExpression", "identifier_path");
+}
