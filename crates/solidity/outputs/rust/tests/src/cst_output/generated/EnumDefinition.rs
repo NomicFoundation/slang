4abede22a1838eab77f304220d8ea4751ad39038
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-02-11 01:13:58 -0300

    fix EnumDefinition grammar + add tests (#318)

---
+++ crates/solidity/outputs/rust/tests/src/cst_output/generated/EnumDefinition.rs
@@ -0,0 +1,14 @@
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
+use crate::cst_output::runner::run;
+use anyhow::Result;
+
+#[test]
+fn multiple_members() -> Result<()> {
+    return run("EnumDefinition", "multiple_members");
+}
+
+#[test]
+fn no_members() -> Result<()> {
+    return run("EnumDefinition", "no_members");
+}
