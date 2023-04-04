621b33838c74415c46ab157205068008e05c5b9b
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-04-04 05:30:19 -0700

    OmarTawfik/add-changesets (#396)
    add changesets cli + cargo publishing
    
    If no issues, merging this PR should trigger a new PR to be created with
    the changeset.
    
    The actual publishing commands are `--dry-run` for now.

---
+++ crates/solidity/outputs/rust/slang_solidity/README.md
@@ -0,0 +1,9 @@
+# Slang
+
+## Solidity compiler tooling by [@NomicFoundation](https://github.com/NomicFoundation)
+
+🔗 [Announcement Post](https://medium.com/nomic-foundation-blog/slang-rethnet-2ad465fd7880)
+
+<br/>
+
+> ❗ This repository is **highly experimental**, and is under active development. Any code or binaries produced from this project should not be used in any production or critical workloads, and is subject to deletion or changes at any time.
---
+++ scripts/release/publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+source "$(dirname "${BASH_SOURCE[0]}")/../_common.sh"
+
+(
+  printf "\n\n🚀 Publishing To Cargo 🚀\n\n\n"
+
+  # TODO: Make sure to pass `CARGO_REGISTRY_TOKEN` when you disable dry-run:
+  _group_output \
+    cargo publish \
+    --all-features \
+    --dry-run \
+    --package "slang_solidity"
+
+  printf "\n\n✅ Published to Cargo Successfully ✅\n\n\n"
+)
+
+(
+  printf "\n\n🚀 Publishing To NPM 🚀\n\n\n"
+
+  # TODO: Make sure to pass `NPM_TOKEN` when you disable dry-run:
+  _group_output \
+    npm publish \
+    --access "public" \
+    --dry-run \
+    --workspace "slang-solidity"
+
+  printf "\n\n✅ Published to NPM Successfully ✅\n\n\n"
+)
