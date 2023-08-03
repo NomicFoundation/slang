283f94529f84f3cf52a119c0a6ec66c409a737da
Igor Matuszewski <xanewok@gmail.com>
2023-08-03 11:24:32 +0200

    Don't emit Cargo markers when loading language definition
    These seem to be redundant: any crate using the
    `solidity_language::SolidityLanguageExtensions` will by rebuilt
    if the `solidity_language` crate changes but it was that package's
    build script that defined the variable that was explicitly depended
    upon by the callers, so no need to manually instruct Cargo here.

--- crates/solidity/inputs/language/src/lib.rs
+++ crates/solidity/inputs/language/src/lib.rs
@@ -1,5 +1,3 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
 use codegen_schema::types::{LanguageDefinition, LanguageDefinitionRef};
 
@@ -7,18 +5,16 @@ pub trait SolidityLanguageExtensions {
     fn load_solidity() -> Result<LanguageDefinitionRef>;
 }
 
+// Set by the build script.
+static LANGUAGE_DEFINITION_BIN: &'static str = env!("SLANG_SOLIDITY_LANGUAGE_DEFINITION_BIN");
+
 impl SolidityLanguageExtensions for LanguageDefinition {
     /// We compile the language definition only once, and then expose it here to all downstream crates.
     /// This ensures that:
     /// 1. Expensive parsing and validation is done only once.
     /// 2. Errors are reported only once, instead of repeating for every crate.
     fn load_solidity() -> Result<LanguageDefinitionRef> {
-        let bin_path = env!("SLANG_SOLIDITY_LANGUAGE_DEFINITION_BIN");
-
-        println!("cargo:rerun-if-changed={bin_path}");
-
-        let bin_path = PathBuf::from(bin_path);
-        let buffer = std::fs::read(&bin_path)?;
+        let buffer = std::fs::read(LANGUAGE_DEFINITION_BIN)?;
         let language: LanguageDefinition = bson::from_slice(&buffer)?;
 
         return Ok(LanguageDefinitionRef::new(language));
