f96abc3c35bcd62db0b50a558c5dc760826c795f
Igor Matuszewski <xanewok@gmail.com>
2023-08-28 17:27:31 +0200

    Simplify and deduplicate ParserFunction logic

--- crates/solidity/outputs/cargo/crate/src/generated/support/parser_function.rs
+++ crates/solidity/outputs/cargo/crate/src/generated/support/parser_function.rs
@@ -1,26 +1,31 @@
 // This file is generated automatically by infrastructure scripts. Please don't edit by hand.
 
+use std::rc::Rc;
+
 use super::{
     super::{cst, kinds::TokenKind, parse_error::ParseError, parse_output::ParseOutput},
     parser_result::*,
     stream::Stream,
 };
 
-// Return type of the function has to be a type parameter of the trait
-pub trait ParserFunction<L, R>
+pub trait ParserFunction<L>
 where
-    Self: Fn(&L, &mut Stream) -> R,
+    Self: Fn(&L, &mut Stream) -> ParserResult,
 {
     fn parse(&self, language: &L, input: &str) -> ParseOutput;
 }
 
-impl<L, F> ParserFunction<L, ParserResult> for F
+impl<L, F> ParserFunction<L> for F
 where
     F: Fn(&L, &mut Stream) -> ParserResult,
 {
     fn parse(&self, language: &L, input: &str) -> ParseOutput {
         let mut stream = Stream::new(input);
-        match self(language, &mut stream) {
+        let result = self(language, &mut stream);
+
+        let is_incomplete = matches!(result, ParserResult::IncompleteMatch(_));
+
+        match result {
             ParserResult::NoMatch(no_match) => ParseOutput {
                 parse_tree: cst::Node::token(TokenKind::SKIPPED, input.to_string()),
                 errors: vec![ParseError::new_covering_range(
@@ -29,57 +34,45 @@ where
                 )],
             },
             ParserResult::IncompleteMatch(IncompleteMatch {
-                mut nodes,
+                nodes,
+                tokens_that_would_have_allowed_more_progress,
+            })
+            | ParserResult::Match(Match {
+                nodes,
                 tokens_that_would_have_allowed_more_progress,
             }) => {
-                if nodes.len() != 1 {
-                    unreachable!(
-                        "IncompleteMatch at the top level of a parser has more than one node"
-                    )
-                }
-                if let cst::Node::Rule(rule_node) = nodes.remove(0) {
-                    let start = stream.position();
+                let topmost_rule = match &nodes[..] {
+                    [cst::Node::Rule(rule)] => Rc::clone(&rule),
+                    [_] => unreachable!(
+                        "(Incomplete)Match at the top level of a parser is not a Rule node"
+                    ),
+                    _ => unreachable!(
+                        "(Incomplete)Match at the top level of a parser does not have exactly one node"
+                    ),
+                };
+
+                let start = stream.position();
+                // Mark the rest of the unconsumed stream as skipped and report an error
+                // NOTE: IncompleteMatch internally consumes the stream when picked via choice,
+                // so needs a separate check here.
+                if start.utf8 < input.len() || is_incomplete {
                     let skipped_node =
                         cst::Node::token(TokenKind::SKIPPED, input[start.utf8..].to_string());
-                    let mut new_children = rule_node.children.clone();
+                    let mut new_children = topmost_rule.children.clone();
                     new_children.push(skipped_node);
+
                     ParseOutput {
-                        parse_tree: cst::Node::rule(rule_node.kind, new_children),
+                        parse_tree: cst::Node::rule(topmost_rule.kind, new_children),
                         errors: vec![ParseError::new_covering_range(
                             start..input.into(),
                             tokens_that_would_have_allowed_more_progress,
                         )],
                     }
                 } else {
-                    unreachable!("IncompleteMatch at the top level of a parser is not a Rule node")
-                }
-            }
-            ParserResult::Match(mut r#match) => {
-                if r#match.nodes.len() != 1 {
-                    unreachable!("Match at the top level of a parser has more than one node")
-                }
-                if let cst::Node::Rule(rule_node) = r#match.nodes.remove(0) {
-                    let start = stream.position();
-                    if start.utf8 < input.len() {
-                        let skipped_node =
-                            cst::Node::token(TokenKind::SKIPPED, input[start.utf8..].to_string());
-                        let mut new_children = rule_node.children.clone();
-                        new_children.push(skipped_node);
-                        ParseOutput {
-                            parse_tree: cst::Node::rule(rule_node.kind, new_children),
-                            errors: vec![ParseError::new_covering_range(
-                                start..input.into(),
-                                r#match.tokens_that_would_have_allowed_more_progress,
-                            )],
-                        }
-                    } else {
-                        ParseOutput {
-                            parse_tree: cst::Node::Rule(rule_node),
-                            errors: vec![],
-                        }
+                    ParseOutput {
+                        parse_tree: cst::Node::Rule(topmost_rule),
+                        errors: vec![],
                     }
-                } else {
-                    unreachable!("Match at the top level of a parser is not a Rule node")
                 }
             }
             ParserResult::PrattOperatorMatch(..) => unreachable!("PrattOperatorMatch is internal"),
