c9132772862833952f7e7c7c5216eff825b19275
Igor Matuszewski <xanewok@gmail.com>
2023-09-05 09:30:58 +0200

    chore: Remove unused now ParserResult::try_recover_with

--- crates/solidity/outputs/npm/crate/src/generated/support/recovery.rs
+++ crates/solidity/outputs/npm/crate/src/generated/support/recovery.rs
@@ -1,42 +1,14 @@
 // This file is generated automatically by infrastructure scripts. Please don't edit by hand.
 
-use crate::cst;
 use crate::kinds::TokenKind;
 use crate::parse_error::ParseError;
 use crate::support::ParserResult;
-use crate::text_index::{TextRange, TextRangeExtensions as _};
+use crate::text_index::TextRangeExtensions as _;
 
 use super::parser_result::SkippedUntil;
 use super::ParserContext;
 
 impl ParserResult {
-    pub fn try_recover_with(
-        self,
-        input: &mut ParserContext,
-        skip_tokens_for_recovery: impl Fn(&mut ParserContext) -> Option<TextRange>,
-    ) -> ParserResult {
-        match self {
-            ParserResult::IncompleteMatch(mut result) => {
-                if let Some(skipped) = skip_tokens_for_recovery(input) {
-                    result.nodes.push(cst::Node::token(
-                        TokenKind::SKIPPED,
-                        input.content(skipped.utf8()),
-                    ));
-
-                    input.emit(ParseError {
-                        text_range: skipped,
-                        tokens_that_would_have_allowed_more_progress: result
-                            .expected_tokens
-                            .clone(),
-                    });
-                }
-
-                ParserResult::r#match(result.nodes, result.expected_tokens)
-            }
-            result => result,
-        }
-    }
-
     pub fn recover_until_with_nested_delims(
         self,
         input: &mut ParserContext,
