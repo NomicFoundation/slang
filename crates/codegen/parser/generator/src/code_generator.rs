389faeb353b0c13f35050fdc2be06805e1da33c4
Igor Matuszewski <xanewok@gmail.com>
2023-09-18 15:43:34 +0200

    cleanup: Use BTreeMap for CodeGenerator::{scanner,parser}_functions

--- crates/codegen/parser/generator/src/code_generator.rs
+++ crates/codegen/parser/generator/src/code_generator.rs
@@ -33,10 +33,10 @@ pub struct CodeGenerator {
     trivia_kinds: BTreeSet<&'static str>,
 
     top_level_scanner_names: BTreeSet<&'static str>,
-    scanner_functions: Vec<(&'static str, String)>, // (name of scanner, code)
+    scanner_functions: BTreeMap<&'static str, String>, // (name of scanner, code)
     scanner_contexts: Vec<ScannerContext>,
 
-    parser_functions: Vec<(&'static str, String)>, // (name of parser, code)
+    parser_functions: BTreeMap<&'static str, String>, // (name of parser, code)
 
     #[serde(skip)]
     scanner_contexts_map: BTreeMap<&'static str, ScannerContext>,
@@ -165,9 +165,6 @@ impl GrammarVisitor for CodeGenerator {
             .map(|(name, scanner)| (*name, scanner.to_scanner_code().to_string()))
             .collect();
 
-        self.parser_functions.sort_by(|a, b| a.0.cmp(b.0));
-        self.scanner_functions.sort_by(|a, b| a.0.cmp(b.0));
-
         for context in self.scanner_contexts_map.values_mut() {
             let mut alpha_literal_trie = Trie::new();
             let mut non_alpha_literal_trie = Trie::new();
@@ -221,7 +218,7 @@ impl GrammarVisitor for CodeGenerator {
         self.production_kinds.insert(parser.name());
         self.rule_kinds.insert(parser.name());
         self.trivia_kinds.insert(parser.name());
-        self.parser_functions.push((
+        self.parser_functions.insert(
             parser.name(),
             {
                 let code = parser.to_parser_code();
@@ -229,7 +226,7 @@ impl GrammarVisitor for CodeGenerator {
                 quote! { #code.with_kind(RuleKind::#rule_kind) }
             }
             .to_string(),
-        ))
+        );
     }
 
     fn parser_definition_enter(&mut self, parser: &ParserDefinitionRef) {
@@ -239,14 +236,14 @@ impl GrammarVisitor for CodeGenerator {
             self.production_kinds.insert(parser.name());
             self.rule_kinds.insert(parser.name());
             let code = parser.to_parser_code();
-            self.parser_functions.push((
+            self.parser_functions.insert(
                 parser.name(),
                 {
                     let rule_kind = format_ident!("{}", parser.name());
                     quote! { #code.with_kind(RuleKind::#rule_kind) }
                 }
                 .to_string(),
-            ));
+            );
         }
     }
 
@@ -257,7 +254,7 @@ impl GrammarVisitor for CodeGenerator {
         for (_, _, name, _) in &parser.node().operators {
             self.rule_kinds.insert(name);
         }
-        self.parser_functions.push((
+        self.parser_functions.insert(
             parser.name(),
             {
                 let code = parser.to_parser_code();
@@ -265,7 +262,7 @@ impl GrammarVisitor for CodeGenerator {
                 quote! { #code.with_kind(RuleKind::#rule_kind) }
             }
             .to_string(),
-        ))
+        );
     }
 
     fn scanner_definition_node_enter(&mut self, node: &ScannerDefinitionNode) {
