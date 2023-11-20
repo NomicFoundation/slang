34a80343dbdbe2042f281ad9d1649ef13f5eeebc
Igor Matuszewski <xanewok@gmail.com>
2023-11-20 17:12:08 +0100

    Prefer `cursor_with_offset(TextIndex::ZERO)`
    ...than `Default::default()`, which doesn't say much at the call-site.

--- crates/solidity/outputs/npm/crate/src/generated/support/parser_function.rs
+++ crates/solidity/outputs/npm/crate/src/generated/support/parser_function.rs
@@ -3,7 +3,10 @@
 use std::rc::Rc;
 
 use super::{
-    super::{cst, kinds::TokenKind, parse_error::ParseError, parse_output::ParseOutput},
+    super::{
+        cst, kinds::TokenKind, parse_error::ParseError, parse_output::ParseOutput,
+        text_index::TextIndex,
+    },
     context::ParserContext,
     parser_result::*,
 };
@@ -97,7 +100,7 @@ where
                     debug_assert_eq!(
                         errors.len() > 0,
                         parse_tree
-                            .cursor_with_offset(Default::default())
+                            .cursor_with_offset(TextIndex::ZERO)
                             .any(|x| x.as_token_with_kind(&[TokenKind::SKIPPED]).is_some())
                     );
 
