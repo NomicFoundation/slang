a22497b5e5ba23de63bbb1f85c9c5079aa6fe5f8
Igor Matuszewski <xanewok@gmail.com>
2023-12-04 10:58:20 +0100

    Add a test for compound tokens

--- crates/solidity/outputs/cargo/tests/src/cst_output/generated/Statements.rs
+++ crates/solidity/outputs/cargo/tests/src/cst_output/generated/Statements.rs
@@ -4,6 +4,11 @@ use crate::cst_output::runner::run;
 use anyhow::Result;
 
 #[test]
+fn compound_tokens() -> Result<()> {
+    return run("Statements", "compound_tokens");
+}
+
+#[test]
 fn invalid_termination() -> Result<()> {
     return run("Statements", "invalid_termination");
 }
