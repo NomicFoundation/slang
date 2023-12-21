80e89f53b022e440007f405662739c3cee7a3563
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2023-12-21 03:08:36 -0800

    remove `ProductionKind` from parser runtime
    remaining from #699

--- crates/codegen/parser/runtime/src/kinds.rs
+++ crates/codegen/parser/runtime/src/kinds.rs
@@ -42,23 +42,6 @@ impl RuleKind {
     }
 }
 
-#[derive(
-    Debug,
-    Eq,
-    Ord,
-    PartialEq,
-    PartialOrd,
-    serde::Serialize,
-    strum_macros::AsRefStr,
-    strum_macros::Display,
-    strum_macros::EnumString,
-)]
-#[cfg_attr( feature = "slang_napi_interfaces", /* derives `Clone` and `Copy` */ napi(string_enum, namespace = "kinds") )]
-#[cfg_attr(not(feature = "slang_napi_interfaces"), derive(Clone, Copy))]
-pub enum ProductionKind {
-    XXX,
-}
-
 /// The lexical context of the scanner.
 #[derive(strum_macros::FromRepr)]
 #[cfg_attr(feature = "slang_napi_interfaces", /* derives `Clone` and `Copy` */ napi(string_enum, namespace = "language"))]
