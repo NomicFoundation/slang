3ed6a03039e6390cf24571d68cdc9a168e063a22
Igor Matuszewski <xanewok@gmail.com>
2024-01-09 13:22:10 +0100

    tests: Add TrailingTrivia CST tests

---
+++ crates/solidity/outputs/cargo/tests/src/cst_output/generated/TrailingTrivia.rs
@@ -0,0 +1,20 @@
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
+use anyhow::Result;
+
+use crate::cst_output::runner::run;
+
+#[test]
+fn multi_line_spanning_multiple_lines() -> Result<()> {
+    run("TrailingTrivia", "multi_line_spanning_multiple_lines")
+}
+
+#[test]
+fn multi_line_without_newline() -> Result<()> {
+    run("TrailingTrivia", "multi_line_without_newline")
+}
+
+#[test]
+fn only_single_comment() -> Result<()> {
+    run("TrailingTrivia", "only_single_comment")
+}
