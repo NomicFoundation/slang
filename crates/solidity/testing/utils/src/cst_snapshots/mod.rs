7abd1c09262a8c61b1fe0a346fc957069aa565cd
OmarTawfik <15987992+OmarTawfik@users.noreply.github.com>
2024-01-09 23:06:53 -0800

    streamline deeply nested CST snapshots

--- crates/solidity/testing/utils/src/cst_snapshots/mod.rs
+++ crates/solidity/testing/utils/src/cst_snapshots/mod.rs
@@ -2,29 +2,15 @@ use std::cmp::max;
 use std::fmt::Write;
 
 use anyhow::Result;
-use slang_solidity::cst;
+use inflector::Inflector;
+use slang_solidity::cst::Node;
 use slang_solidity::cursor::CursorWithNames;
-use slang_solidity::kinds::TokenKind;
-use slang_solidity::text_index::{TextRange, TextRangeExtensions};
-
-use crate::node_extensions::NodeExtensions;
-
-/// Whether to include the whitespace in the snapshot test CST output.
-#[derive(Clone, Copy)]
-pub enum WithWhitespace {
-    Yes,
-    No,
-}
+use slang_solidity::text_index::TextRangeExtensions;
 
 pub struct CstSnapshots;
 
 impl CstSnapshots {
-    pub fn render(
-        source: &str,
-        errors: &Vec<String>,
-        cursor: CursorWithNames,
-        with_whitespace: WithWhitespace,
-    ) -> Result<String> {
+    pub fn render(source: &str, errors: &Vec<String>, cursor: CursorWithNames) -> Result<String> {
         let mut w = String::new();
 
         write_source(&mut w, source)?;
@@ -33,7 +19,7 @@ impl CstSnapshots {
         write_errors(&mut w, errors)?;
         writeln!(&mut w)?;
 
-        write_tree(&mut w, cursor, source, with_whitespace)?;
+        write_tree(&mut w, cursor, source)?;
 
         Ok(w)
     }
@@ -97,104 +83,108 @@ fn write_errors<W: Write>(w: &mut W, errors: &Vec<String>) -> Result<()> {
     Ok(())
 }
 
-fn write_tree<W: Write>(
-    w: &mut W,
-    mut cursor: CursorWithNames,
-    source: &str,
-    with_whitespace: WithWhitespace,
-) -> Result<()> {
-    let with_whitespace = matches!(with_whitespace, WithWhitespace::Yes);
+fn write_tree<W: Write>(w: &mut W, mut cursor: CursorWithNames, source: &str) -> Result<()> {
+    writeln!(w, "Tree:")?;
+    write_node(w, &mut cursor, source, 0)?;
 
-    write!(w, "Tree:")?;
-    writeln!(w)?;
-
-    let significant_nodes_with_range = std::iter::from_fn(|| loop {
-        let (depth, range) = (cursor.depth(), cursor.text_range());
-
-        // Skip whitespace and trivia rules containing only those tokens
-        match cursor.next() {
-            Some(cst::NamedNode {
-                name: _,
-                node: cst::Node::Rule(rule),
-            }) if rule.is_trivia()
-                && !with_whitespace
-                && rule.children.iter().all(|named| {
-                    named
-                        .node
-                        .as_token()
-                        .map_or(false, |t| is_whitespace(t.kind))
-                }) =>
-            {
-                continue
-            }
-            Some(cst::NamedNode {
-                name: _,
-                node: cst::Node::Token(token),
-            }) if !with_whitespace && is_whitespace(token.kind) => continue,
-            next => break next.map(|item| (item, depth, range)),
-        }
-    });
-
-    for (node, depth, range) in significant_nodes_with_range {
-        write_node(w, &node, &range, source, depth)?;
-    }
-
+    assert!(!cursor.go_to_next());
     Ok(())
 }
 
 fn write_node<W: Write>(
     w: &mut W,
-    cst::NamedNode { name, node }: &cst::NamedNode,
-    range: &TextRange,
+    cursor: &mut CursorWithNames,
     source: &str,
-    indentation: usize,
+    depth: usize,
 ) -> Result<()> {
-    let range_string = format!("{range:?}", range = range.utf8());
+    let indentation = " ".repeat(4 * depth);
+    write!(w, "{indentation}  - ")?;
 
-    let (node_value, node_comment) = if range.is_empty() {
-        let preview = match node {
-            cst::Node::Rule(_) if !node.children().is_empty() => "",
-            cst::Node::Rule(_) => " []",
-            cst::Node::Token(_) => " \"\"",
-        };
+    loop {
+        let key = render_key(cursor);
+        write!(w, "{key}")?;
 
-        (preview.to_owned(), range_string)
-    } else {
-        let preview = render_source_preview(source, range)?;
+        // combine parents with a single child on the same line:
+        if cursor.node().children().len() == 1 {
+            let parent_range = cursor.text_range();
+            assert!(cursor.go_to_next());
 
-        if node.children().is_empty() {
-            // "foo" # 1..2
-            (format!(" {preview}"), range_string)
-        } else {
-            // # 1..2 "foo"
-            (String::new(), format!("{range_string} {preview}"))
+            let child_range = cursor.text_range();
+            assert_eq!(parent_range, child_range);
+
+            write!(w, " ► ")?;
+            continue;
         }
-    };
 
-    let field_name = name.map(|name| format!("({name}) ")).unwrap_or_default();
+        break;
+    }
 
-    let name = match node {
-        cst::Node::Rule(rule) => format!("{field_name}{}", rule.kind.as_ref()),
-        cst::Node::Token(token) => format!("{field_name}{}", token.kind.as_ref()),
-    };
+    let value = render_value(cursor, source)?;
+    if value.is_empty() {
+        write!(w, ":")?;
+    } else {
+        write!(w, ": {value}")?;
+    }
 
-    writeln!(
-        w,
-        "{indentation}  - {name}:{node_value} # {node_comment}",
-        indentation = " ".repeat(4 * indentation),
-    )?;
+    let comment = render_comment(cursor, source)?;
+    writeln!(w, " # {comment}")?;
+
+    for _ in cursor.node().children() {
+        assert!(cursor.go_to_next());
+        write_node(w, cursor, source, depth + 1)?;
+    }
 
     Ok(())
 }
 
-pub fn render_source_preview(source: &str, range: &TextRange) -> Result<String> {
-    let max_length = 50;
-    let length = range.end.utf8 - range.start.utf8;
+fn render_key(cursor: &mut CursorWithNames) -> String {
+    let kind = match cursor.node() {
+        Node::Rule(rule) => rule.kind.to_string(),
+        Node::Token(token) => token.kind.to_string(),
+    };
+
+    if let Some(name) = cursor.node_name() {
+        format!("({name}꞉ {kind})", name = name.as_ref().to_snake_case())
+    } else {
+        format!("({kind})")
+    }
+}
+
+fn render_value(cursor: &mut CursorWithNames, source: &str) -> Result<String> {
+    let value = match cursor.node() {
+        Node::Rule(rule) if rule.children.is_empty() => "[]".to_string(),
+        Node::Rule(_) => String::new(),
+        Node::Token(_) => render_source_preview(cursor, source)?,
+    };
+
+    Ok(value)
+}
+
+fn render_comment(cursor: &mut CursorWithNames, source: &str) -> Result<String> {
+    let range = cursor.text_range();
+    let range_string = format!("{range:?}", range = range.utf8());
+
+    let comment = match cursor.node() {
+        Node::Token(_) => range_string,
+        Node::Rule(_) if range.is_empty() => range_string,
+        Node::Rule(_) => {
+            let source_preview = render_source_preview(cursor, source)?;
+            format!("{range_string} {source_preview}")
+        }
+    };
+
+    Ok(comment)
+}
+
+fn render_source_preview(cursor: &mut CursorWithNames, source: &str) -> Result<String> {
+    let range = cursor.text_range().utf8();
+    let length = range.len();
 
     // Trim long values:
+    let max_length = 50;
     let contents = source
         .bytes()
-        .skip(range.start.utf8)
+        .skip(range.start)
         .take(length.clamp(0, max_length))
         .collect();
 
@@ -211,19 +201,13 @@ pub fn render_source_preview(source: &str, range: &TextRange) -> Result<String> 
         .replace('\n', "\\n");
 
     // Surround by quotes for use in yaml:
-    let contents = {
-        if contents.contains('"') {
-            let contents = contents.replace('\'', "''");
-            format!("'{contents}'")
-        } else {
-            let contents = contents.replace('"', "\\\"");
-            format!("\"{contents}\"")
-        }
+    let contents = if contents.contains('"') {
+        let contents = contents.replace('\'', "''");
+        format!("'{contents}'")
+    } else {
+        let contents = contents.replace('"', "\\\"");
+        format!("\"{contents}\"")
     };
 
     Ok(contents)
-}
-
-fn is_whitespace(kind: TokenKind) -> bool {
-    matches!(kind, TokenKind::Whitespace | TokenKind::EndOfLine)
 }
