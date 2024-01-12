c192b116ed0e7fd55f5729baf21869e45e29957e
Igor Matuszewski <xanewok@gmail.com>
2024-01-12 19:38:40 +0100

    refactor: Generate shell completions as part of `infra setup`

--- crates/infra/cli/src/commands/setup/mod.rs
+++ crates/infra/cli/src/commands/setup/mod.rs
@@ -2,8 +2,12 @@ mod cargo;
 mod npm;
 mod pipenv;
 
+use std::path::PathBuf;
+
 use anyhow::Result;
-use clap::{Parser, ValueEnum};
+use clap::{CommandFactory, Parser, ValueEnum};
+use clap_complete::Shell;
+use infra_utils::paths::PathExtensions;
 
 use crate::commands::setup::cargo::setup_cargo;
 use crate::commands::setup::npm::setup_npm;
@@ -24,6 +28,8 @@ impl SetupController {
 
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum)]
 enum SetupCommand {
+    /// Generate shell completions for this CLI.
+    ShellCompletions,
     /// Install Cargo dependencies.
     Cargo,
     /// Install NPM dependencies.
@@ -37,9 +43,32 @@ impl OrderedCommand for SetupCommand {
         Terminal::step(format!("setup {name}", name = self.clap_name()));
 
         match self {
+            SetupCommand::ShellCompletions => generate_shell_completions(),
             SetupCommand::Cargo => setup_cargo(),
             SetupCommand::Npm => setup_npm(),
             SetupCommand::Pipenv => setup_pipenv(),
         }
     }
+}
+
+fn generate_shell_completions() -> Result<()> {
+    println!("Generating shell completions...\n");
+
+    let target_dir = std::env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
+    let shell_completions_dir = PathBuf::from(target_dir).join("shell-completions");
+
+    std::fs::create_dir_all(&shell_completions_dir)?;
+
+    let mut cmd = crate::commands::Cli::command();
+    let bin_name = cmd.get_bin_name().expect("Set by #[command]").to_owned();
+
+    for shell in [Shell::Bash, Shell::Zsh] {
+        let file = clap_complete::generate_to(shell, &mut cmd, &bin_name, &shell_completions_dir)?;
+
+        let canonicalized = file.canonicalize()?;
+        let stripped = canonicalized.strip_repo_root()?;
+        println!("Generated file at {}", stripped.display());
+    }
+
+    Ok(())
 }
