f447483459f2bc48054af321a146950a814a9c5a
Igor Matuszewski <xanewok@gmail.com>
2024-01-15 20:40:10 +0100

    fixup

--- crates/solidity/outputs/npm/crate/build.rs
+++ crates/solidity/outputs/npm/crate/build.rs
@@ -5,7 +5,6 @@ use slang_solidity as _;
 #[cfg(not(feature = "slang_napi_interfaces"))]
 compile_error!("The whole point is to enable slang_napi_interfaces for this cdylib crate!");
 
-
 fn main() {
     napi_build::setup();
 }
