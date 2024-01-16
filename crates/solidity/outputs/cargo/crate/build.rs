0ffb71657106c92616dcb63c777d4798cdb6a27e
OmarTawfik <15987992+OmarTawfik@users.noreply.github.com>
2024-01-16 15:12:31 -0800

    move `GrammarConstructorDslV2` call to `codegen_parser`

--- crates/solidity/outputs/cargo/crate/build.rs
+++ crates/solidity/outputs/cargo/crate/build.rs
@@ -2,19 +2,15 @@
 //! It is removed when publishing to crates.io.
 
 use anyhow::Result;
-use codegen_grammar::{Grammar, GrammarConstructorDslV2};
-use codegen_parser_generator::{AstModel, RustGenerator};
+use codegen_parser_generator::RustGenerator;
 use infra_utils::cargo::CargoWorkspace;
 use solidity_language::SolidityDefinition;
 
 fn main() -> Result<()> {
     let language = SolidityDefinition::create();
-    let grammar = Grammar::from_dsl_v2(&language);
-    let ast_model = AstModel::create(&language);
 
     RustGenerator::generate(
-        &grammar,
-        &ast_model,
+        &language,
         &CargoWorkspace::locate_source_crate("slang_solidity")?.join("src/generated"),
     )
 }
