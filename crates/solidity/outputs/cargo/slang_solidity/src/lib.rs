f55eb7426ecaa9e3cc600e89033f8ea132791fdf
Igor Matuszewski <Xanewok@gmail.com>
2024-02-14 21:58:56 +0100

    Streamline cdylib setup for Node.js addon (#745)
    Follow up to #735 
    
    I scratched the NAPI build itch, as I never fully understood the root
    issue. As I did, in the meantime I tried to better document and show
    what the core issue really is
    (https://github.com/rust-lang/cargo/issues/8628), so that future readers
    do not have to get down that rabbit hole themselves.
    
    Unfortunately, it's not entirely possible to eliminate the workaround
    without patching napi-rs to support a different workaround (build cdylib
    as example; see the link above). However, I tried to eliminate some of
    the redundant setup not to confuse what's actually needed; for example,
    we never build codegen_parser_runtime into `cdylib` but only use the
    source inside and similarly the `solidity_npm_crate` (renamed to
    `slang_solidity_node_addon` in
    https://github.com/NomicFoundation/slang/commit/8853229c8f8db1966d4a4926b5ea3fd79afc21f7)
    can only be ever compiled as `cdylib` and is not intended to be used as
    a regular Rust `rlib`.
    
    See the comments added in the diff and in the commits for more context.

--- crates/solidity/outputs/cargo/slang_solidity/src/lib.rs
+++ crates/solidity/outputs/cargo/slang_solidity/src/lib.rs
@@ -9,11 +9,3 @@ pub use generated::*;
 mod supress_cli_dependencies {
     use {anyhow as _, clap as _, serde_json as _};
 }
-
-// Make sure the NAPI crate is rebuilt when the Rust crate is updated.
-// Unfortunately, the NAPI types cannot be enabled in 'slang_solidity' directly, as 'napi build' crashes
-// when they are imported into other crates, so 'solidity_npm_crate' cannot have any dependants.
-#[cfg(feature = "slang_napi_interfaces")]
-mod supress_napi_dependencies {
-    use slang_solidity as _;
-}
