b2a6ef4c5058f551492dc338eed3f8aaa93fae79
Igor Matuszewski <xanewok@gmail.com>
2024-03-05 12:18:37 +0100

    tests: Add escape_{lf,cr,crlf}_{double,single}_quote tests

--- crates/solidity/outputs/cargo/tests/src/cst_output/generated/string_literal.rs
+++ crates/solidity/outputs/cargo/tests/src/cst_output/generated/string_literal.rs
@@ -5,18 +5,33 @@ use anyhow::Result;
 use crate::cst_output::runner::run;
 
 #[test]
-fn escape_newline_double_quote() -> Result<()> {
-    run("StringLiteral", "escape_newline_double_quote")
+fn escape_cr_double_quote() -> Result<()> {
+    run("StringLiteral", "escape_cr_double_quote")
 }
 
 #[test]
-fn escape_newline_single_quote() -> Result<()> {
-    run("StringLiteral", "escape_newline_single_quote")
+fn escape_cr_single_quote() -> Result<()> {
+    run("StringLiteral", "escape_cr_single_quote")
 }
 
 #[test]
-fn escape_newline_single_quote_crlf() -> Result<()> {
-    run("StringLiteral", "escape_newline_single_quote_crlf")
+fn escape_crlf_double_quote() -> Result<()> {
+    run("StringLiteral", "escape_crlf_double_quote")
+}
+
+#[test]
+fn escape_crlf_single_quote() -> Result<()> {
+    run("StringLiteral", "escape_crlf_single_quote")
+}
+
+#[test]
+fn escape_lf_double_quote() -> Result<()> {
+    run("StringLiteral", "escape_lf_double_quote")
+}
+
+#[test]
+fn escape_lf_single_quote() -> Result<()> {
+    run("StringLiteral", "escape_lf_single_quote")
 }
 
 #[test]
