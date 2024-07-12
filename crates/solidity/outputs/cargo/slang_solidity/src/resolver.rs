b64e544779b6caca79f8093c5c561847b9df361e
Gustavo Giráldez <ggiraldez@manas.tech>
2024-07-12 19:26:02 -0400

    Avoid I/O in the import resolver when resolving relative imports
    (Direct imports are still not handled)

--- crates/solidity/outputs/cargo/slang_solidity/src/resolver.rs
+++ crates/solidity/outputs/cargo/slang_solidity/src/resolver.rs
@@ -1,22 +1,85 @@
-use std::path::PathBuf;
-
 use metaslang_bindings::PathResolver;
 
 pub struct SolidityPathResolver;
 
 impl PathResolver for SolidityPathResolver {
-    fn resolve_path(&self, context_path_str: &str, path_to_resolve_str: &str) -> Option<String> {
-        let path_to_resolve = PathBuf::from(path_to_resolve_str);
-        if path_to_resolve.is_relative() {
-            PathBuf::from(context_path_str)
-                .parent()
-                .map(|parent| parent.join(path_to_resolve))
-                .and_then(|resolved| resolved.canonicalize().ok())
-                .map(|resolved| resolved.to_string_lossy().to_string())
+    fn resolve_path(&self, context_path: &str, path_to_resolve: &str) -> Option<String> {
+        if is_relative_path(path_to_resolve) {
+            // Relative import: the actual path will be computed using the
+            // context path (ie. the path of the importing source unit)
+            normalize_path(path_to_resolve, get_parent_path(context_path))
         } else {
-            // TODO: for file paths this needs to try to apply a base path, and
-            // then all include paths in order
-            Some(path_to_resolve_str.to_string())
+            // Direct import: this path will be used as-is
+            // TODO: implement the VFS concept and use a resolver callback if
+            // the path is not already present
+            Some(path_to_resolve.to_string())
         }
+    }
+}
+
+fn is_relative_path(path: &str) -> bool {
+    path.starts_with("./") || path.starts_with("../")
+}
+
+fn get_parent_path(path: &str) -> &str {
+    let sep_index = path.rfind('/').map_or(0usize, |index| index + 1);
+    &path[..sep_index]
+}
+
+fn normalize_path(path: &str, base_path: &str) -> Option<String> {
+    let mut normalized = base_path.to_string();
+    let mut path = path;
+    while let Some(index) = path.find('/') {
+        match &path[..=index] {
+            "/" | "./" => (),
+            "../" => {
+                if normalized.is_empty() {
+                    return None;
+                }
+                let last_sep = normalized[..normalized.len() - 1]
+                    .rfind('/')
+                    .unwrap_or(0usize);
+                normalized.drain(last_sep..);
+            }
+            other => normalized.push_str(other),
+        }
+        path = &path[index + 1..];
+    }
+    normalized.push_str(path);
+    Some(normalized)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_parent_path() {
+        assert_eq!("", get_parent_path(""));
+        assert_eq!("", get_parent_path("foo.sol"));
+        assert_eq!("bar/", get_parent_path("bar/foo.sol"));
+    }
+
+    #[test]
+    fn test_normalize_path() {
+        assert_eq!("foo.sol", normalize_path("foo.sol", "").unwrap());
+        assert_eq!("bar/foo.sol", normalize_path("foo.sol", "bar/").unwrap());
+        assert_eq!("bar/foo.sol", normalize_path("./foo.sol", "bar/").unwrap());
+        assert_eq!("foo.sol", normalize_path("../foo.sol", "bar/").unwrap());
+        assert_eq!("foo.sol", normalize_path("./../foo.sol", "bar/").unwrap());
+        assert_eq!(
+            "foo.sol",
+            normalize_path("../../foo.sol", "bar/baz/").unwrap()
+        );
+        assert_eq!(
+            "foo.sol",
+            normalize_path(".././../foo.sol", "bar/baz/").unwrap()
+        );
+        assert_eq!(
+            "baz/foo.sol",
+            normalize_path("../baz/foo.sol", "bar/").unwrap()
+        );
+        assert!(normalize_path("../foo.sol", "").is_none());
+        assert!(normalize_path("../../foo.sol", "bar/").is_none());
     }
 }
