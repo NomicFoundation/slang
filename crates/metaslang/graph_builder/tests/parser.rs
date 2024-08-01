9689b21cf24730cf78a9b04fca2e03fbef970c05
Gustavo Giráldez <ggiraldez@manas.tech>
2024-08-01 14:54:14 -0400

    Check for query error messages in graph builder parser tests

--- crates/metaslang/graph_builder/tests/parser.rs
+++ crates/metaslang/graph_builder/tests/parser.rs
@@ -1583,6 +1583,7 @@ fn query_parse_errors_have_file_location() {
         Err(ParseError::QueryError(e)) => e,
         Err(e) => panic!("Unexpected error: {e}"),
     };
+    assert_eq!(err.message, "Parse error:\n'NonExistingNode' is not a valid node kind at: NonExistingNode ] ]\n        \n");
     assert_eq!(err.row, 2, "expected row 2, got {}", err.row);
     assert_eq!(err.column, 19, "expected column 19, got {}", err.column);
     // assert_eq!(err.offset, 48, "expected offset 48, got {}", err.offset);
@@ -1604,6 +1605,7 @@ fn multiline_query_parse_errors_have_file_location() {
         Err(ParseError::QueryError(e)) => e,
         Err(e) => panic!("Unexpected error: {e}"),
     };
+    assert_eq!(err.message, "Parse error:\n'NonExistingNode' is not a valid node kind at: NonExistingNode ] ]\n        )\n        \n");
     assert_eq!(err.row, 5, "expected row 5, got {}", err.row);
     assert_eq!(err.column, 23, "expected column 23, got {}", err.column);
     // assert_eq!(err.offset, 112, "expected offset 112, got {}", err.offset);
