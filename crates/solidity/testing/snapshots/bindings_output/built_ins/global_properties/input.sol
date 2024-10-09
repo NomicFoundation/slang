59a751838b83d83465adbabab52f46d132a4ce89
Gustavo Giráldez <ggiraldez@manas.tech>
2024-10-09 16:42:43 -0400

    Test `block` global variable and its members

--- crates/solidity/testing/snapshots/bindings_output/built_ins/global_properties/input.sol
+++ crates/solidity/testing/snapshots/bindings_output/built_ins/global_properties/input.sol
@@ -1,8 +1,18 @@
 contract Test {
-    uint last_time;
-    function foo() public {
+    function testTx() public {
         address origin = tx.origin;
-        last_time = now;
         uint price = tx.gasprice;
+    }
+
+    function testBlock() public {
+        uint v1 = block.basefee;
+        uint v2 = block.blobbasefee;
+        uint v3 = block.chainid;
+        address v4 = block.coinbase;
+        uint v5 = block.difficulty;
+        uint v6 = block.gaslimit;
+        uint v7 = block.number;
+        uint v8 = block.prevrandao;
+        uint v9 = block.timestamp;
     }
 }
