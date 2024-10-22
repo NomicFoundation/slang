5ddbba6e98dfe9299769ab771cfc0edc8deb99a7
Gustavo Giráldez <ggiraldez@manas.tech>
2024-10-22 13:15:17 -0400

    Update generated built-in Solidity files

--- crates/solidity/outputs/cargo/crate/src/generated/bindings/generated/built_ins/0.8.24.sol
+++ crates/solidity/outputs/cargo/crate/src/generated/bindings/generated/built_ins/0.8.24.sol
@@ -1,4 +1,5 @@
-// This file is generated automatically. Do not edit by hand.
+// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
+
 contract $BuiltIns$ {
   function addmod(uint x, uint y, uint k) public returns (uint);
   function assert(bool condition) public;
