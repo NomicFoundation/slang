6e3fc5de9b30e8f820732c9ba57e1d8707f7a6f2
Gustavo Giráldez <ggiraldez@manas.tech>
2024-11-29 12:47:15 -0500

    Compose the binding rules file from parts

--- crates/testlang/outputs/cargo/crate/src/generated/bindings/generated/binding_rules.rs
+++ crates/testlang/outputs/cargo/crate/src/generated/bindings/generated/binding_rules.rs
@@ -3,6 +3,5 @@
 #[allow(clippy::needless_raw_string_hashes)]
 #[allow(dead_code)] // TODO(#982): use to create the graph
 pub const BINDING_RULES_SOURCE: &str = r#####"
-    // TODO(#982): add rules for TestLang here...
-
+    
 "#####;
