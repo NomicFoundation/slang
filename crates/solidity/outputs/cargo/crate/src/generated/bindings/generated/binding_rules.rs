f4961a7ca6fbfad874d374b2ed2ad2c176a79059
Gustavo Giráldez <ggiraldez@manas.tech>
2024-12-02 14:54:26 -0500

    Fix and clarify comment

--- crates/solidity/outputs/cargo/crate/src/generated/bindings/generated/binding_rules.rs
+++ crates/solidity/outputs/cargo/crate/src/generated/bindings/generated/binding_rules.rs
@@ -853,7 +853,9 @@ inherit .lexical_scope
   ; resolve the value type through our scope
   edge @value_type.type_ref -> @mapping.lexical_scope
 
-  ; We use the value_type's definition path as our, although that's not correct
+  ; We use the value_type's definition path as our own because it's needed when
+  ; a mapping is the target of a `using` directive. It's not correct, but we
+  ; don't have the analog referencing path either.
   let @mapping.pop_begin = @value_type.pop_begin
   let @mapping.pop_end = @value_type.pop_end
 }
