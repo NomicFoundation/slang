8cac89b9c1243dbf3115f778b19bebc620fa603d
Mitch Russell <34431383+mjoerussell@users.noreply.github.com>
2024-12-02 13:43:08 -0600

    Generate Typescript documentation from wit files (#1157)
    This PR uses the `jco types` command to generate Typescript definitions
    for the wasm crates instead of `jco transpile`. The reason is that `jco
    transpile` generates ts definitions by decoding WASM component binaries,
    whereas `jco types` parses .wit files directly and uses that
    information. This means that the `Resolve` instance created during
    `types` has access to the type documentation info, and the one created
    during `transpile` does not.
    
    I've modified `infra check npm` with a new step which calls `jco types`
    and handles the resulting .d.ts files. When calling `jco transpile`
    we're now passing the option `--no-typescript` to avoid generating
    conflicting typescript outputs.
    
    These changes depend on https://github.com/NomicFoundation/jco/pull/1,
    which augments the `types` command to accept the same configuration file
    that we pass to `transpile`. Without this, the codegen in `types` will
    not be able to generate Typescript enums for Rust enums, or any other
    customization that we want.

--- crates/codegen/runtime/npm/package/wasm/generated/interfaces/wasi-clocks-wall-clock.d.ts
+++
@@ -1,7 +0,0 @@
-// This file is generated automatically by infrastructure scripts. Please don't edit by hand.
-
-export namespace WasiClocksWallClock {}
-export interface Datetime {
-  seconds: bigint;
-  nanoseconds: number;
-}
