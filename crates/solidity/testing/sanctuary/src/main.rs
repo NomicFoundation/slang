b42b49dd44a0578beb056c3bb6299f909f942b49
Gustavo Giráldez <ggiraldez@manas.tech>
2024-12-03 13:14:38 -0500

    Show combined results from all shards at the end of a Sanctuary run

--- crates/solidity/testing/sanctuary/src/main.rs
+++ crates/solidity/testing/sanctuary/src/main.rs
@@ -2,17 +2,20 @@ mod chains;
 mod datasets;
 mod events;
 mod reporting;
+mod results;
 mod tests;
 
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::PathBuf;
 
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use infra_utils::github::GitHub;
 use infra_utils::paths::PathExtensions;
 use infra_utils::terminal::{NumbersDefaultDisplay, Terminal};
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+use results::{display_all_results, AllResults};
 
 use crate::chains::Chain;
 use crate::datasets::{DataSet, SourceFile};
@@ -21,6 +24,18 @@ use crate::tests::{run_test, select_tests, TestSelection};
 
 #[derive(Debug, Parser)]
 struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Test(TestCommand),
+    ShowCombinedResults(ShowCombinedResultsCommand),
+}
+
+#[derive(Debug, Parser)]
+struct TestCommand {
     /// Chain and sub-network to run against.
     #[command(subcommand)]
     chain: Chain,
@@ -48,13 +63,29 @@ struct ShardingOptions {
     shard_index: Option<usize>,
 }
 
+#[derive(Debug, Parser)]
+struct ShowCombinedResultsCommand {
+    results_file: PathBuf,
+}
+
 fn main() -> Result<()> {
-    let Cli {
+    let Cli { command } = Cli::parse();
+
+    match command {
+        Commands::Test(test_command) => run_test_command(test_command),
+        Commands::ShowCombinedResults(show_command) => {
+            run_show_combined_results_command(show_command)
+        }
+    }
+}
+
+fn run_test_command(command: TestCommand) -> Result<()> {
+    let TestCommand {
         chain,
         sharding_options,
         trace,
         check_bindings,
-    } = Cli::parse();
+    } = command;
 
     Terminal::step(format!(
         "initialize {chain}/{network}",
@@ -106,11 +137,11 @@ fn main() -> Result<()> {
                 .map(|index| index.to_string())
                 .unwrap_or_default()
         );
-        let value = serde_json::to_string(&events)?;
+        let results = events.to_results();
+        let value = serde_json::to_string(&results)?;
 
         let mut output_file = OpenOptions::new()
             .create(true)
-            .write(true)
             .append(true)
             .open(output_file)?;
 
@@ -153,6 +184,15 @@ fn run_in_parallel(files: &Vec<SourceFile>, events: &Events, check_bindings: boo
     .par_iter()
     .panic_fuse(/* Halt as soon as possible if a child panics */)
     .try_for_each(|file| run_test(file, events, check_bindings))
+}
+
+fn run_show_combined_results_command(command: ShowCombinedResultsCommand) -> Result<()> {
+    let ShowCombinedResultsCommand { results_file } = command;
+
+    let contents = String::from_utf8(std::fs::read(results_file)?)?;
+    let all_results: AllResults = serde_json::from_str(&contents)?;
+    display_all_results(&all_results);
+    Ok(())
 }
 
 #[test]
