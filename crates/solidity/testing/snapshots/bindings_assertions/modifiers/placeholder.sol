9ed010d275c8411622939fc413c8c67525c212e9
Gustavo Giráldez <ggiraldez@manas.tech>
2024-12-16 18:57:24 -0500

    Migrate some binding assertions tests to snapshots

--- crates/solidity/testing/snapshots/bindings_assertions/modifiers/placeholder.sol
+++
@@ -1,6 +0,0 @@
-contract Test {
-    function test() public {
-        _;
-        //<ref:!   -- the placeholder statement does not bind outside of a modifier
-    }
-}
--- crates/solidity/testing/snapshots/bindings_output/interfaces/visibility/input.sol
+++ crates/solidity/testing/snapshots/bindings_output/interfaces/visibility/input.sol
@@ -1,11 +1,15 @@
 interface Example {
     enum Choice { One, Two }
     struct Book { string author; }
-    function calculate() returns (Choice);
-    function get_book() returns (Book);
+    function calculate() external returns (int);
+    function get_book() external returns (Book memory);
 }
 
 contract Test {
     Example.Choice choice = Example.Choice.One;
     Example.Book book;
+
+    function test(Example e) public returns (int) {
+        return e.calculate();
+    }
 }
