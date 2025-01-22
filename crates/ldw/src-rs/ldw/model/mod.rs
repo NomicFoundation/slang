e06de536b1671ea8aabfb6a49207485e21a2b2e2
mjoerussell <mjoerussell@gmail.com>
2025-01-22 16:31:56 -0600

    Remove ldw/src-rs - not needed for slang

--- crates/ldw/src-rs/ldw/model/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod analysed;
-pub mod parsed;
-pub mod resolved;
--- crates/ldw/src-rs/ldw/model/resolved/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod builder;
-pub mod handler;
-pub mod in_place_transformer;
-pub mod model;
-pub mod transformer;
-pub mod visitor;
