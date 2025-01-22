8f08eed7b668569aff81da4933248823bc58224e
mjoerussell <mjoerussell@gmail.com>
2025-01-22 15:56:51 -0600

    * Add `infra setup ldw` command, which installs the required npm packages for ldw * Remove bespoke devtools in ldw dir

---
+++ crates/infra/cli/src/commands/setup/ldw/mod.rs
@@ -0,0 +1,11 @@
+use infra_utils::commands::Command;
+use infra_utils::paths::PathExtensions;
+use std::path::Path;
+
+/// Install npm packages for ldw.
+pub fn setup_ldw() {
+    Command::new("npm")
+        .current_dir(Path::repo_path("crates/ldw"))
+        .arg("install")
+        .run();
+}
--- crates/ldw/scripts/setup-rust.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "### Installing Rust"
-rustup default 1.80.0
-echo
