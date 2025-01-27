f63609aff35b1ccfa5be25cf3f99a7edaf589978
mjoerussell <mjoerussell@gmail.com>
2025-01-27 14:47:24 -0600

    Running `ldw process-model` in the solidity build script.
    Processing the model is currently broken because of some unimplemented features in ldw.

--- crates/solidity/outputs/cargo/crate/build.rs
+++ crates/solidity/outputs/cargo/crate/build.rs
@@ -1,9 +1,11 @@
 //! This build script is only used for local development.
 //! It is removed when publishing to crates.io.
 
+use std::path::Path;
+
 use anyhow::Result;
 use codegen_runtime_generator::RuntimeGenerator;
-use infra_utils::cargo::CargoWorkspace;
+use infra_utils::{cargo::CargoWorkspace, commands::Command, paths::PathExtensions};
 use solidity_language::{render_built_ins, SolidityDefinition};
 
 fn main() -> Result<()> {
@@ -12,7 +14,8 @@ fn main() -> Result<()> {
     let input_dir =
         CargoWorkspace::locate_source_crate("codegen_runtime_cargo_crate")?.join("src/runtime");
 
-    let output_dir = CargoWorkspace::locate_source_crate("slang_solidity")?.join("src/generated");
+    let crate_path = CargoWorkspace::locate_source_crate("slang_solidity")?;
+    let output_dir = crate_path.join("src/generated");
 
     let mut fs = RuntimeGenerator::generate_product(&language, &input_dir, &output_dir)?;
     let built_ins_output_dir = output_dir.join("bindings/generated/built_ins");
@@ -21,5 +24,27 @@ fn main() -> Result<()> {
         &language,
         &built_ins_output_dir,
         render_built_ins,
-    )
+    )?;
+
+    Command::new("ts-node")
+        .current_dir(Path::repo_path("crates/codegen/ldw"))
+        .args(["-P", "./tsconfig.json"])
+        .arg("src-ts/cli/ldw.ts")
+        .arg("process-model")
+        .args([
+            "--in-dir",
+            crate_path
+                .join("src/generated/ldw-models/")
+                .to_str()
+                .unwrap(),
+        ])
+        .args([
+            "--out-dir",
+            crate_path.join("src/generated/ldw/").to_str().unwrap(),
+        ])
+        .args(["--language", "rust"])
+        .args(["--name", "l0::generated"])
+        .run();
+
+    Ok(())
 }
