82a24b682c745794b507d35e81dbb42d7b401abd
Omar Tawfik <15987992+OmarTawfik@users.noreply.github.com>
2025-02-04 05:34:47 -0800

    fix tests

--- crates/solidity/testing/perf/src/tests/query.rs
+++ crates/solidity/testing/perf/src/tests/query.rs
@@ -29,7 +29,7 @@ pub fn run(files: Vec<ParsedFile>) {
     }
 
     assert_eq!(
-        functions_count, 232,
+        functions_count, 233,
         "Failed to fetch all function definitions"
     );
 }
