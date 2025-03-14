e9269d5dea6df59038b1e068d54a73a2a821a378
Gustavo Giráldez <ggiraldez@manas.tech>
2025-03-13 21:36:04 -0400

    Remove code generation for bindings assertions

--- crates/codegen/testing/src/lib.rs
+++ crates/codegen/testing/src/lib.rs
@@ -1,4 +1,3 @@
-mod bindings_assertions;
 mod bindings_output;
 mod common;
 mod cst_output;
@@ -9,17 +8,11 @@ use anyhow::Result;
 use codegen_language_definition::model::Language;
 use common::generate_version_breaks;
 
-use crate::bindings_assertions::generate_bindings_assertions_tests;
 use crate::bindings_output::generate_bindings_output_tests;
 use crate::cst_output::generate_cst_output_tests;
 
 pub trait TestingGeneratorExtensions {
     fn generate_version_breaks(&self, lang_def_dir: &Path, output_dir: &Path) -> Result<()>;
-    fn generate_bindings_assertions_tests(
-        &self,
-        sources_dir: &Path,
-        output_dir: &Path,
-    ) -> Result<()>;
     fn generate_bindings_output_tests(&self, snapshots_dir: &Path, output_dir: &Path)
         -> Result<()>;
     fn generate_cst_output_tests(&self, snapshots_dir: &Path, output_dir: &Path) -> Result<()>;
@@ -28,9 +21,6 @@ pub trait TestingGeneratorExtensions {
 impl TestingGeneratorExtensions for Language {
     fn generate_version_breaks(&self, lang_def_dir: &Path, output_dir: &Path) -> Result<()> {
         generate_version_breaks(self, lang_def_dir, output_dir)
-    }
-    fn generate_bindings_assertions_tests(&self, data_dir: &Path, output_dir: &Path) -> Result<()> {
-        generate_bindings_assertions_tests(data_dir, output_dir)
     }
     fn generate_bindings_output_tests(&self, data_dir: &Path, output_dir: &Path) -> Result<()> {
         generate_bindings_output_tests(data_dir, output_dir)
