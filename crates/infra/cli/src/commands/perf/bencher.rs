81cc0f5b490881438a0b5da27241c9e51f7fb42f
Beta Ziliani <beta@manas.tech>
2025-05-15 11:06:15 -0300

    moving comment

--- crates/infra/cli/src/commands/perf/bencher.rs
+++ crates/infra/cli/src/commands/perf/bencher.rs
@@ -1,13 +1,13 @@
 use infra_utils::commands::Command;
 use infra_utils::github::GitHub;
 
+// Use a dummy test token for dry runs:
+// https://github.com/bencherdev/bencher/issues/468
 // Source: https://github.com/bencherdev/bencher/blob/aa31a002842cfb0da9d6c60569396fc5261f5111/tasks/test_api/src/task/test/smoke_test.rs#L20
 const BENCHER_TEST_TOKEN: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhcGlfa2V5IiwiZXhwIjo1OTkzNjQyMTU2LCJpYXQiOjE2OTg2NzQ4NjEsImlzcyI6Imh0dHBzOi8vZGV2ZWwtLWJlbmNoZXIubmV0bGlmeS5hcHAvIiwic3ViIjoibXVyaWVsLmJhZ2dlQG5vd2hlcmUuY29tIiwib3JnIjpudWxsfQ.9z7jmM53TcVzc1inDxTeX9_OR0PQPpZAsKsCE7lWHfo";
 
 pub(crate) fn run_bench(dry_run: bool, adapter: &str, test_runner: &str) {
     let token = if dry_run {
-        // Use a dummy test token for dry runs:
-        // https://github.com/bencherdev/bencher/issues/468
         BENCHER_TEST_TOKEN.to_string()
     } else {
         std::env::var("BENCHER_API_TOKEN").expect(
