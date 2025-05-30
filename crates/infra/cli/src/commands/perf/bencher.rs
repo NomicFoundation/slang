210940fcaca508ea99830538c10215771d16e0bb
Beta Ziliani <beta@manas.tech>
2025-05-30 09:04:52 -0300

    Parametric bencher project

--- crates/infra/cli/src/commands/perf/bencher.rs
+++ crates/infra/cli/src/commands/perf/bencher.rs
@@ -6,7 +6,12 @@ use infra_utils::github::GitHub;
 // Source: https://github.com/bencherdev/bencher/blob/aa31a002842cfb0da9d6c60569396fc5261f5111/tasks/test_api/src/task/test/smoke_test.rs#L20
 const BENCHER_TEST_TOKEN: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhcGlfa2V5IiwiZXhwIjo1OTkzNjQyMTU2LCJpYXQiOjE2OTg2NzQ4NjEsImlzcyI6Imh0dHBzOi8vZGV2ZWwtLWJlbmNoZXIubmV0bGlmeS5hcHAvIiwic3ViIjoibXVyaWVsLmJhZ2dlQG5vd2hlcmUuY29tIiwib3JnIjpudWxsfQ.9z7jmM53TcVzc1inDxTeX9_OR0PQPpZAsKsCE7lWHfo";
 
-const BENCHER_PROJECT: &str = "slang-extended";
+// Default bencher project. Can be overriden with the env var SLANG_BENCHER_PROJECT
+const BENCHER_PROJECT: &str = "slang-extended-dashboard";
+
+fn get_bencher_project() -> String {
+    std::env::var("SLANG_BENCHER_PROJECT").unwrap_or(String::from(BENCHER_PROJECT))
+}
 
 pub(crate) fn run_bench(dry_run: bool, adapter: &str, test_runner: &str) {
     let token = if dry_run {
@@ -23,9 +28,11 @@ pub(crate) fn run_bench(dry_run: bool, adapter: &str, test_runner: &str) {
         "dev"
     };
 
+    let project = get_bencher_project();
+
     let mut command = Command::new("bencher")
         .arg("run")
-        .property("--project", BENCHER_PROJECT)
+        .property("--project", &project)
         .property("--adapter", adapter)
         .property("--testbed", testbed)
         .secret("BENCHER_API_TOKEN", token);
@@ -41,7 +48,7 @@ pub(crate) fn run_bench(dry_run: bool, adapter: &str, test_runner: &str) {
         "
 
 Bencher Run is complete...
-Test Results: [https://bencher.dev/console/projects/{BENCHER_PROJECT}/reports]
+Test Results: [https://bencher.dev/console/projects/{project}/reports]
 "
     );
 }
