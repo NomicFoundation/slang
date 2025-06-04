1c1dfe7e70cfc77d9c894bfe6bb99e9672605e0f
Gustavo Giráldez <ggiraldez@manas.tech>
2025-06-04 16:49:23 -0400

    Resolve constants and built-ins inside nested Yul functions

--- crates/solidity/outputs/cargo/tests/src/bindings_output/generated/yul.rs
+++ crates/solidity/outputs/cargo/tests/src/bindings_output/generated/yul.rs
@@ -97,6 +97,11 @@ fn modifier_params() -> Result<()> {
 }
 
 #[test]
+fn nested_functions() -> Result<()> {
+    run(T, "nested_functions")
+}
+
+#[test]
 fn slot_offset_members() -> Result<()> {
     run(T, "slot_offset_members")
 }
