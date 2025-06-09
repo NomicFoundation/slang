d444f518fafd7211f811dd7d05debb9fb38d08bf
Beta Ziliani <beta@manas.tech>
2025-06-09 14:44:44 -0300

    Missing bits from previous commit

--- crates/codegen/runtime/cargo/crate/src/runtime/parser/parser_support/separated_helper.rs
+++ crates/codegen/runtime/cargo/crate/src/runtime/parser/parser_support/separated_helper.rs
@@ -29,6 +29,7 @@ impl SeparatedHelper {
                             {
                                 ParserResult::Match(r#match) => {
                                     accum.extend(r#match.nodes);
+                                    continue;
                                 }
                                 _ => unreachable!("We just checked that the separator matches"),
                             }
@@ -71,6 +72,7 @@ impl SeparatedHelper {
                             {
                                 ParserResult::Match(r#match) => {
                                     accum.extend(r#match.nodes);
+                                    continue;
                                 }
                                 _ => unreachable!("We just checked that the separator matches"),
                             }
