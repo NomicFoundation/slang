29e960ac94f502a207a9a9712701f6112a21c119
Beta Ziliani <beta@manas.tech>
2025-06-09 16:01:06 -0300

    finishing refactor: perf/npm-benches is perf/npm

--- crates/infra/cli/src/commands/perf/npm/mod.rs
+++ crates/infra/cli/src/commands/perf/npm/mod.rs
@@ -32,7 +32,7 @@ impl NpmController {
     fn execute_npm_benchmarks(&self) {
         let test_runner = format!(
             "cargo run --package {package} -- --pattern=\"{pattern}\" --cold={cold} --hot={hot}",
-            package = "solidity_testing_perf_npmbenches",
+            package = "solidity_testing_perf_npm",
             pattern = &self.pattern,
             cold = &self.cold,
             hot = &self.hot,
