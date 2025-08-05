9a64ef919117709811eec4133465efc4c6ebca8c
Gustavo Giráldez <ggiraldez@manas.tech>
2025-08-05 19:08:35 -0400

    Allow also comparing references with stack graph binder output

--- crates/solidity/outputs/cargo/tests/src/binder/renderer.rs
+++ crates/solidity/outputs/cargo/tests/src/binder/renderer.rs
@@ -64,6 +64,7 @@ pub(crate) fn binder_report(
             &mut report,
             binder_data,
             &all_definitions,
+            &all_references,
             &definitions_by_id,
         )?;
     }
@@ -213,11 +214,41 @@ fn report_binding_graph_differences(
     report: &mut String,
     binder_data: &Output,
     all_definitions: &[CollectedDefinition],
+    all_references: &[CollectedReference],
     definitions_by_id: &HashMap<NodeId, usize>,
 ) -> Result<()> {
-    let binding_graph = binder_data.compilation_unit.binding_graph();
+    let has_diff =
+        diff_binding_graph_definitions(report, binder_data, all_definitions, definitions_by_id)?
+            || diff_binding_graph_references(
+                report,
+                binder_data,
+                all_references,
+                definitions_by_id,
+            )?;
 
+    writeln!(report)?;
+    if has_diff {
+        writeln!(
+            report,
+            "Definitions and/or references from binding graph and new binder DIFFER"
+        )?;
+    } else {
+        writeln!(
+            report,
+            "No differences found in definitions or references from the binding graph"
+        )?;
+    }
+    Ok(())
+}
+
+fn diff_binding_graph_definitions(
+    report: &mut String,
+    binder_data: &Output,
+    all_definitions: &[CollectedDefinition],
+    definitions_by_id: &HashMap<NodeId, usize>,
+) -> Result<bool> {
     let mut has_diff = false;
+    let binding_graph = binder_data.compilation_unit.binding_graph();
     let mut binder_definitions = definitions_by_id
         .keys()
         .copied()
@@ -240,25 +271,111 @@ fn report_binding_graph_differences(
         let definition = &all_definitions[report_id - 1];
         writeln!(
             report,
-            "{definition} not found in new stack graph output",
+            "{definition} not found in stack graph output",
             definition = definition.display(binder_data),
         )?;
         has_diff = true;
     }
 
-    writeln!(report)?;
-    if has_diff {
-        writeln!(
-            report,
-            "Definitions from binding graph and new binder DIFFER"
-        )?;
-    } else {
-        writeln!(
-            report,
-            "No differences found in definitions from the binding graph"
-        )?;
+    Ok(has_diff)
+}
+
+fn diff_binding_graph_references(
+    report: &mut String,
+    binder_data: &Output,
+    all_references: &[CollectedReference],
+    definitions_by_id: &HashMap<NodeId, usize>,
+) -> Result<bool> {
+    let mut has_diff = false;
+    let binding_graph = binder_data.compilation_unit.binding_graph();
+    let mut binder_references = all_references
+        .iter()
+        .map(|reference| (reference.cursor.node().id(), reference))
+        .collect::<HashMap<_, _>>();
+
+    for graph_reference in binding_graph.all_references() {
+        if graph_reference.get_file().is_built_ins() {
+            continue;
+        }
+        let id = graph_reference.id();
+        let Some(binder_reference) = binder_references.remove(&id) else {
+            has_diff = true;
+            writeln!(report, "{graph_reference} not found in new binder output")?;
+            continue;
+        };
+        let definitions = graph_reference.definitions();
+        match &binder_reference.resolution {
+            Resolution::Unresolved => {
+                if !definitions.is_empty() {
+                    has_diff = true;
+                    writeln!(report, "{graph_reference} failed to resolve in new binder")?;
+                }
+            }
+            Resolution::Definition(node_id) => match definitions.len() {
+                1 => {
+                    if definitions[0].id() != *node_id {
+                        has_diff = true;
+                        writeln!(
+                            report,
+                            "{reference} resolved to a different definition",
+                            reference = binder_reference.display(definitions_by_id)
+                        )?;
+                    }
+                }
+                0 => {
+                    has_diff = true;
+                    writeln!(
+                        report,
+                        "{reference} resolved but did not in stack graph output",
+                        reference = binder_reference.display(definitions_by_id)
+                    )?;
+                }
+                _ => {
+                    has_diff = true;
+                    writeln!(
+                        report,
+                        "{reference} resolved uniquely but resolved ambiguously in stack graph output",
+                        reference = binder_reference.display(definitions_by_id)
+                    )?;
+                }
+            },
+            Resolution::Ambiguous(node_ids) => {
+                if node_ids.len() != definitions.len() {
+                    has_diff = true;
+                    writeln!(
+                        report,
+                        "{reference} has a different ambiguous resolution than {graph_reference}",
+                        reference = binder_reference.display(definitions_by_id)
+                    )?;
+                }
+            }
+            Resolution::BuiltIn(_) => {
+                if definitions.is_empty()
+                    || definitions
+                        .iter()
+                        .all(|definition| definition.get_file().is_user())
+                {
+                    has_diff = true;
+                    writeln!(
+                        report,
+                        "{reference} does not resolve to a built-in in the stack graph output",
+                        reference = binder_reference.display(definitions_by_id)
+                    )?;
+                }
+            }
+        }
     }
-    Ok(())
+
+    for reference in binder_references.values() {
+        writeln!(
+            report,
+            "{reference} not found in stack graph output",
+            reference = reference.display(definitions_by_id)
+        )?;
+        has_diff = true;
+    }
+
+    Ok(has_diff)
 }
 
 // Collection of definitions, references and unbound identifiers
