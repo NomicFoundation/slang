56593a96791dec9ea24fac8854de25771bb237e8
Beta Ziliani <beta@manas.tech>
2025-08-21 10:28:31 -0300

    Removing unnecessary check

--- crates/solidity/testing/perf/cargo/src/tests/parser.rs
+++ crates/solidity/testing/perf/cargo/src/tests/parser.rs
@@ -1,7 +1,6 @@
 use std::rc::Rc;
 
 use slang_solidity::compilation::CompilationUnit;
-use slang_solidity::diagnostic::{Diagnostic, Severity};
 use slang_solidity::parser::ParseError;
 
 use crate::compilation_builder::CompilationBuilder;
@@ -17,12 +16,7 @@ pub fn run(project: &'static SolidityProject) -> Rc<CompilationUnit> {
     let unit = builder.build().unwrap();
     assert_ne!(unit.files().len(), 0);
     for file in unit.files() {
-        let errors: Vec<_> = file
-            .errors()
-            .iter()
-            .filter(|e| e.severity() == Severity::Error)
-            .collect();
-        debug_assert_eq!(errors, Vec::<&ParseError>::new());
+        debug_assert_eq!(file.errors(), &Vec::<ParseError>::new());
     }
     unit.into()
 }
