ab61709bb02b45a6fd9f752d7636c32dd71c2020
Beta Ziliani <beta@manas.tech>
2025-08-21 10:57:58 -0300

    better assertion

--- crates/solidity/testing/perf/cargo/src/tests/parser.rs
+++ crates/solidity/testing/perf/cargo/src/tests/parser.rs
@@ -1,7 +1,6 @@
 use std::rc::Rc;
 
 use slang_solidity::compilation::CompilationUnit;
-use slang_solidity::parser::ParseError;
 
 use crate::compilation_builder::CompilationBuilder;
 use crate::dataset::SolidityProject;
@@ -16,7 +15,7 @@ pub fn run(project: &'static SolidityProject) -> Rc<CompilationUnit> {
     let unit = builder.build().unwrap();
     assert_ne!(unit.files().len(), 0);
     for file in unit.files() {
-        assert_eq!(file.errors(), &Vec::<ParseError>::new());
+        assert!(file.errors().is_empty());
     }
     unit.into()
 }
