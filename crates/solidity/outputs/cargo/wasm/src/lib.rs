4722143e7b4a7a29650ca66534db10cb525a0a28
Beta Ziliani <beta.ziliani@gmail.com>
2025-09-17 12:46:37 -0300

    applying suggestion from code review

--- crates/solidity/outputs/cargo/wasm/src/lib.rs
+++ crates/solidity/outputs/cargo/wasm/src/lib.rs
@@ -1,5 +1,5 @@
 // We generate functions prefixed with `_` in order to avoid having to be forced to use them.
 #![allow(clippy::used_underscore_items)]
 
-pub mod wasm_crate;
+mod wasm_crate;
 use slang_solidity as rust_crate;
