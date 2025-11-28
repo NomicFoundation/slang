e5a514e37b68cd733334dc1e647679ce15457ad1
Teodoro Freund <tfreund95@gmail.com>
2025-11-28 11:28:29 +0000

    Improve a lot the formatting:
    - now terminals include the span and a copy of the string (easy for now)
    - There's an indirection, inlining those is critical for performance
    - It seems, as a rule, it's worth inlining things that have a single rule and a single matching element

--- crates/solidity-v2/outputs/cargo/parser/src/parser/tests/mod.rs
+++ crates/solidity-v2/outputs/cargo/parser/src/parser/tests/mod.rs
@@ -7,22 +7,22 @@ use crate::lexer::contexts::ContextKind;
 use crate::lexer::definition::Lexer;
 use crate::parser::grammar;
 
-fn test(source: &str, expected: &str) {
+fn test(source: &str, _expected: &str) {
     let kind = ContextKind::Solidity;
     let version = LanguageVersion::V0_8_30;
     {
         // This is just to check for now
-        let mut lexer = Lexer::new(kind, source.clone(), version);
+        let lexer = Lexer::new(kind, source, version);
         for l in lexer {
             println!("{:?}", l);
         }
         println!("Finished lexing");
     }
 
-    let mut lexer = Lexer::new(kind, source.clone(), version);
+    let lexer = Lexer::new(kind, source, version);
 
     let result = grammar::SourceUnitParser::new().parse(&source, lexer);
-    if (result.is_err()) {
+    if result.is_err() {
         println!("{:?}", result);
     }
     // Only checking if it's ok for now, at least until the representation of the AST is fixed
