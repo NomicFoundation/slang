fbb9738f7c62012824b3a49d60da0c802a646b33
Teodoro Freund <tfreund95@gmail.com>
2025-12-03 16:51:16 +0000

    Added passthrough options to infra test commands

--- crates/infra/cli/src/commands/test/mod.rs
+++ crates/infra/cli/src/commands/test/mod.rs
@@ -1,48 +1,52 @@
 use anyhow::Result;
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand};
 use infra_utils::cargo::{CargoWorkspace, CargoWorkspaceCommands};
 use infra_utils::commands::Command;
 use infra_utils::terminal::Terminal;
 
-use crate::utils::{ClapExtensions, OrderedCommand};
-
 #[derive(Clone, Debug, Default, Parser)]
 pub struct TestController {
-    #[clap(trailing_var_arg = true)]
-    commands: Vec<TestCommand>,
+    #[clap(subcommand)]
+    commands: Option<TestCommand>,
 }
 
 impl TestController {
     pub fn execute(&self) -> Result<()> {
-        TestCommand::execute_in_order(&self.commands)
-    }
-}
-
-#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum)]
-enum TestCommand {
-    /// Run 'cargo test' for all crates, features, and targets.
-    Cargo,
-    /// Run 'test' scripts in each NPM package in the repository.
-    Npm,
-}
-
-impl OrderedCommand for TestCommand {
-    fn execute(&self) -> Result<()> {
-        Terminal::step(format!("test {name}", name = self.clap_name()));
-
-        match self {
-            TestCommand::Cargo => test_cargo()?,
-            TestCommand::Npm => test_npm(),
-        }
-
+        match &self.commands {
+            Some(TestCommand::Cargo { passthrough }) => test_cargo()?.args(passthrough).run(),
+            Some(TestCommand::Npm { passthrough }) => test_npm().args(passthrough).run(),
+            None => {
+                test_cargo()?.run();
+                test_npm().run();
+            }
+        };
         Ok(())
     }
 }
 
-fn test_cargo() -> Result<()> {
+#[derive(Clone, Debug, Subcommand)]
+enum TestCommand {
+    /// Run 'cargo test' for all crates, features, and targets.
+    Cargo {
+        #[arg(
+            trailing_var_arg = true,
+            help = "Passthrough arguments to cargo nextest."
+        )]
+        passthrough: Vec<String>,
+    },
+    /// Run 'test' scripts in each NPM package in the repository.
+    Npm {
+        #[arg(trailing_var_arg = true, help = "Passthrough arguments to jest.")]
+        passthrough: Vec<String>,
+    },
+}
+
+fn test_cargo() -> Result<Command> {
+    Terminal::step("test Cargo");
+
     CargoWorkspace::install_binary("cargo-nextest")?;
 
-    Command::new("cargo")
+    Ok(Command::new("cargo")
         .args(["nextest", "run"])
         .flag("--workspace")
         .flag("--all-features")
@@ -51,15 +55,13 @@ fn test_cargo() -> Result<()> {
         .flag("--bins")
         .flag("--examples")
         .flag("--no-fail-fast")
-        .add_build_rustflags()
-        .run();
-
-    Ok(())
+        .add_build_rustflags())
 }
 
-fn test_npm() {
+fn test_npm() -> Command {
+    Terminal::step("test Npm");
+
     Command::new("jest")
         .env("NODE_OPTIONS", "--experimental-vm-modules") // because we are executing ESM tests
         .env("NODE_NO_WARNINGS", "1") // disable warnings about experimental feature above (too much noise)
-        .run();
 }
