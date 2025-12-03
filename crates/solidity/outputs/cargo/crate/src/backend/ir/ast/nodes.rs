49d4b4496b93646f94dec7ecd471d2ddf09545ab
Gustavo Giráldez <ggiraldez@manas.tech>
2025-12-03 16:53:47 -0500

    Allow converting from `Definition` into the appropriate AST node types

--- crates/solidity/outputs/cargo/crate/src/backend/ir/ast/nodes.rs
+++ crates/solidity/outputs/cargo/crate/src/backend/ir/ast/nodes.rs
@@ -96,6 +96,29 @@ macro_rules! define_variant {
             pub fn [<is_ $variant:snake:lower>](&self) -> bool {
                 matches!(self.internal_definition(), binder::Definition::$variant(_))
             }
+
+            pub fn [<as_ $variant:snake:lower>](&self) -> Option<[<$variant Definition>]> {
+                if let binder::Definition::$variant(variant) = self.internal_definition() {
+                    Some(Rc::new([<$variant DefinitionStruct>]::create(&variant.ir_node, &self.semantic)))
+                } else {
+                    None
+                }
+            }
+        }
+    };
+    ($variant:ident, $deftype:ident) => {
+        paste! {
+            pub fn [<is_ $variant:snake:lower>](&self) -> bool {
+                matches!(self.internal_definition(), binder::Definition::$variant(_))
+            }
+
+            pub fn [<as_ $variant:snake:lower>](&self) -> Option<$deftype> {
+                if let binder::Definition::$variant(variant) = self.internal_definition() {
+                    Some(Rc::new([<$deftype Struct>]::create(&variant.ir_node, &self.semantic)))
+                } else {
+                    None
+                }
+            }
         }
     };
 }
@@ -122,26 +145,26 @@ impl DefinitionStruct {
     define_variant!(Constant);
     define_variant!(Contract);
     define_variant!(Enum);
-    define_variant!(EnumMember);
+    define_variant!(EnumMember, Identifier);
     define_variant!(Error);
     define_variant!(Event);
     define_variant!(Function);
-    define_variant!(Import);
-    define_variant!(ImportedSymbol);
+    define_variant!(Import, PathImport);
+    define_variant!(ImportedSymbol, ImportDeconstructionSymbol);
     define_variant!(Interface);
     define_variant!(Library);
-    define_variant!(Modifier);
-    define_variant!(Parameter);
+    define_variant!(Modifier, FunctionDefinition);
+    define_variant!(Parameter, Parameter);
     define_variant!(StateVariable);
     define_variant!(Struct);
-    define_variant!(StructMember);
-    define_variant!(TypeParameter);
+    define_variant!(StructMember, StructMember);
+    define_variant!(TypeParameter, Parameter);
     define_variant!(UserDefinedValueType);
-    define_variant!(Variable);
+    define_variant!(Variable, VariableDeclarationStatement);
     define_variant!(YulFunction);
-    define_variant!(YulLabel);
-    define_variant!(YulParameter);
-    define_variant!(YulVariable);
+    define_variant!(YulLabel, YulLabel);
+    define_variant!(YulParameter, Identifier);
+    define_variant!(YulVariable, Identifier);
 }
 
 include!("ast_nodes.generated.rs");
