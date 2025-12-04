48d3e9dde3e4b7af61b1b6c9569580f651451d14
Gustavo Giráldez <ggiraldez@manas.tech>
2025-12-04 10:48:20 -0500

    Retrieve AST trees from the `SemanticAnalysis` object

--- crates/solidity/outputs/cargo/crate/src/backend/ir/ast/nodes.rs
+++ crates/solidity/outputs/cargo/crate/src/backend/ir/ast/nodes.rs
@@ -170,6 +170,12 @@ impl DefinitionStruct {
 include!("ast_nodes.generated.rs");
 
 impl SourceUnitStruct {
+    pub fn file_id(&self) -> String {
+        self.semantic
+            .node_id_to_file_id(self.ir_node.node_id)
+            .unwrap()
+    }
+
     pub fn contracts(&self) -> impl Iterator<Item = ContractDefinition> + use<'_> {
         self.members()
             .filter_map(|member| member.as_contract_definition())
