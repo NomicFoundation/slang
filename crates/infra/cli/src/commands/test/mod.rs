962ca4dfa8fcf4000678b2ca98d06967ba188998
Teodoro Freund <tfreund95@gmail.com>
2025-12-05 16:31:35 +0000

    Added passthrough options to `infra test` commands (#1478)
    While working locally it's useful to execute a few tests rather than the
    whole testsuite, both `nextest` and `jest` offer options to filter
    tests, either by name or by more complex patterns.
    
    This PR adds the possibility to pass arguments to these tests engines
    from our `infra test {cargo, npm}` commands, for example `infra test
    cargo tuple` will run all of the cargo tests whose name matches
    `"tuple"`.
    
    It also allows to use other flags from these test engines, for example
    we could want for `nextest` to not capture any output, and show
    everything on stdout, by specifying `--no-capture`: `infra test cargo
    tuple --no-capture`.
    
    `infra test` will run all NPM and Cargo tests, without passing any extra
    arguments.
    *Note:* One caveat is that we no longer allow to enumerate all test
    suites (ie `infra test cargo npm` is no longer allowed).

--- crates/infra/cli/src/commands/test/mod.rs
+++ crates/infra/cli/src/commands/test/mod.rs
@@ -1,45 +1,51 @@
+use std::iter::empty;
+
 use anyhow::Result;
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand};
 use infra_utils::cargo::{CargoWorkspace, CargoWorkspaceCommands};
 use infra_utils::commands::Command;
 use infra_utils::terminal::Terminal;
 
-use crate::utils::{ClapExtensions, OrderedCommand};
-
 #[derive(Clone, Debug, Default, Parser)]
 pub struct TestController {
-    #[clap(trailing_var_arg = true)]
-    commands: Vec<TestCommand>,
+    #[clap(subcommand)]
+    command: Option<TestCommand>,
 }
 
 impl TestController {
     pub fn execute(&self) -> Result<()> {
-        TestCommand::execute_in_order(&self.commands)
-    }
-}
-
-#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum)]
-enum TestCommand {
-    /// Run 'cargo test' for all crates, features, and targets.
-    Cargo,
-    /// Run 'test' scripts in each NPM package in the repository.
-    Npm,
-}
-
-impl OrderedCommand for TestCommand {
-    fn execute(&self) -> Result<()> {
-        Terminal::step(format!("test {name}", name = self.clap_name()));
-
-        match self {
-            TestCommand::Cargo => test_cargo()?,
-            TestCommand::Npm => test_npm(),
+        match &self.command {
+            Some(TestCommand::Cargo { passthrough }) => test_cargo(passthrough)?,
+            Some(TestCommand::Npm { passthrough }) => test_npm(passthrough),
+            None => {
+                test_cargo(empty::<String>())?;
+                test_npm(empty::<String>());
+            }
         }
-
         Ok(())
     }
 }
 
-fn test_cargo() -> Result<()> {
+#[derive(Clone, Debug, Subcommand)]
+enum TestCommand {
+    /// Run 'cargo test' for all crates, features, and targets.
+    Cargo {
+        #[arg(
+            trailing_var_arg = true,
+            help = "Passthrough arguments to cargo nextest."
+        )]
+        passthrough: Vec<String>,
+    },
+    /// Run 'test' scripts in each NPM package in the repository.
+    Npm {
+        #[arg(trailing_var_arg = true, help = "Passthrough arguments to jest.")]
+        passthrough: Vec<String>,
+    },
+}
+
+fn test_cargo(passthrough: impl IntoIterator<Item = impl Into<String>>) -> Result<()> {
+    Terminal::step("test Cargo");
+
     CargoWorkspace::install_binary("cargo-nextest")?;
 
     Command::new("cargo")
@@ -52,14 +58,18 @@ fn test_cargo() -> Result<()> {
         .flag("--examples")
         .flag("--no-fail-fast")
         .add_build_rustflags()
+        .args(passthrough)
         .run();
 
     Ok(())
 }
 
-fn test_npm() {
+fn test_npm(passthrough: impl IntoIterator<Item = impl Into<String>>) {
+    Terminal::step("test Npm");
+
     Command::new("jest")
         .env("NODE_OPTIONS", "--experimental-vm-modules") // because we are executing ESM tests
         .env("NODE_NO_WARNINGS", "1") // disable warnings about experimental feature above (too much noise)
+        .args(passthrough)
         .run();
 }
