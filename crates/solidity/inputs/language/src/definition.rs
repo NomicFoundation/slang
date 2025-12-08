f03516dc62873eec15743889006c3ad03a9cb87b
Teodoro Freund <tfreund95@gmail.com>
2025-12-08 13:59:20 +0000

    Add support for Solidity 0.8.31 (#1479)
    Add support for Solidity 0.8.31
    
    - Added new Yul builtin `clz`
    
    
    https://www.soliditylang.org/blog/2025/12/03/solidity-0.8.31-release-announcement

--- crates/solidity/inputs/language/src/definition.rs
+++ crates/solidity/inputs/language/src/definition.rs
@@ -27,7 +27,7 @@ language_macros::compile!(Language(
         "0.8.2", "0.8.3", "0.8.4", "0.8.5", "0.8.6", "0.8.7", "0.8.8", "0.8.9", "0.8.10", "0.8.11",
         "0.8.12", "0.8.13", "0.8.14", "0.8.15", "0.8.16", "0.8.17", "0.8.18", "0.8.19", "0.8.20",
         "0.8.21", "0.8.22", "0.8.23", "0.8.24", "0.8.25", "0.8.26", "0.8.27", "0.8.28", "0.8.29",
-        "0.8.30"
+        "0.8.30", "0.8.31"
     ],
     sections = [
         Section(
@@ -7193,6 +7193,12 @@ language_macros::compile!(Language(
                     name = "mcopy",
                     parameters = ["uint256 t", "uint256 f", "uint256 s"],
                     enabled = From("0.8.24")
+                ),
+                BuiltInFunction(
+                    name = "clz",
+                    parameters = ["uint256 x"],
+                    return_type = "uint256",
+                    enabled = From("0.8.31")
                 )
             ]
         )
