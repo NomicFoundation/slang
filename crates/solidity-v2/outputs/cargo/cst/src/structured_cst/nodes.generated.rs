414f177eef082f7265cb1834e9bf170ee41aca1d
Teodoro Freund <tfreund95@gmail.com>
2026-01-19 12:35:18 +0000

    Rebased and regenerated

--- crates/solidity-v2/outputs/cargo/cst/src/structured_cst/nodes.generated.rs
+++ crates/solidity-v2/outputs/cargo/cst/src/structured_cst/nodes.generated.rs
@@ -2026,7 +2026,7 @@ pub type RevertStatement<'arena> = Box<'arena, RevertStatementStruct<'arena>>;
 #[derive(Debug)]
 pub struct RevertStatementStruct<'arena> {
     pub revert_keyword: RevertKeyword<'arena>,
-    pub error: Option<IdentifierPath<'arena>>,
+    pub error: IdentifierPath<'arena>,
     pub arguments: ArgumentsDeclaration<'arena>,
     pub semicolon: Semicolon<'arena>,
 }
@@ -2034,7 +2034,7 @@ pub struct RevertStatementStruct<'arena> {
 pub fn new_revert_statement<'arena>(
     arena: &'arena Bump,
     revert_keyword: RevertKeyword<'arena>,
-    error: Option<IdentifierPath<'arena>>,
+    error: IdentifierPath<'arena>,
     arguments: ArgumentsDeclaration<'arena>,
     semicolon: Semicolon<'arena>,
 ) -> RevertStatement<'arena> {
