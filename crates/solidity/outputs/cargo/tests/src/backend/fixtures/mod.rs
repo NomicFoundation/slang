de0c541d5eefafe288a12bda66cf0e16a5783c96
Gustavo Giráldez <ggiraldez@manas.tech>
2026-02-05 14:17:19 -0500

    Retain references to `ImportDeconstructionSymbol`s instead of following to the aliased definition (#1510)
    If an identifier resolves lexically to an `ImportDeconstructionSymbol`
    definition, register that instead of skipping it and registering to the
    actual aliased definition. This allows keeping better track of
    referenced imports, which would otherwise appear to have no references
    pointing to them.
    
    The drawback is that we need to explicitly follow through the aliases
    whenever we need access to the actual definition, eg. when resolving
    types, members, etc.
    
    ---------
    
    Co-authored-by: Teodoro Freund <tfreund95@gmail.com>

--- crates/solidity/outputs/cargo/tests/src/backend/fixtures/mod.rs
+++ crates/solidity/outputs/cargo/tests/src/backend/fixtures/mod.rs
@@ -1,10 +1,21 @@
 use anyhow::Result;
 
+mod chained_imports;
 mod counter;
 mod overrides;
 
+pub(crate) use chained_imports::ChainedImports;
 pub(crate) use counter::Counter;
 pub(crate) use overrides::Overrides;
+
+#[test]
+fn test_build_chained_imports_fixture() -> Result<()> {
+    let unit = ChainedImports::build_compilation_unit()?;
+    let semantic_analysis = unit.semantic_analysis();
+    assert_eq!(3, semantic_analysis.files().len());
+
+    Ok(())
+}
 
 #[test]
 fn test_build_counter_fixture() -> Result<()> {
