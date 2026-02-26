d99c83fe51d0e139b4ab0bd31f2045b84962a7c5
Teodoro Freund <tfreund95@gmail.com>
2026-02-26 10:00:57 +0000

    Fixed lints

--- crates/solidity-v2/testing/utils/src/reporting/diagnostic/implementations.rs
+++ crates/solidity-v2/testing/utils/src/reporting/diagnostic/implementations.rs
@@ -7,7 +7,7 @@ use super::{Diagnostic, Severity};
 impl Diagnostic for ParserError {
     fn text_range(&self) -> TextRange {
         match self {
-            ParserError::UnexpectedEOF { location, .. } => {
+            ParserError::UnexpectedEof { location, .. } => {
                 TextRange::from_bytes_range(*location..*location)
             }
             ParserError::UnexpectedToken { range, .. } => range.clone(),
@@ -21,7 +21,7 @@ impl Diagnostic for ParserError {
 
     fn message(&self) -> String {
         match self {
-            ParserError::UnexpectedEOF { expected, .. } => {
+            ParserError::UnexpectedEof { expected, .. } => {
                 format!(
                     "Unexpected end of file. One of {expected_list} was expected",
                     expected_list = expected.iter().map(|e| format!("{e}")).join(", ")
