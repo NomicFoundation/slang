4543dd2e05857f23e59cebb3ef6a500c2fc187e4
Teodoro Freund <tfreund95@gmail.com>
2026-03-06 09:05:37 +0000

    Fixing eol on some test files

--- crates/solidity-v2/testing/snapshots/cst_output/from_v1/StringLiteral/escape_crlf_single_quote/input/generated/input.sol
+++ crates/solidity-v2/testing/snapshots/cst_output/from_v1/StringLiteral/escape_crlf_single_quote/input/generated/input.sol
@@ -3,8 +3,8 @@
 contract C {
     function f() {
 // >>> Copied from crates/solidity/testing/snapshots/cst_output/StringLiteral/escape_crlf_single_quote/input.sol
-'foo\
-bar'
+'foo\
+bar'
 
 // <<<
 ;
