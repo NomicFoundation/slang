8651d7469d97e73547fd000fa6f77201ab23dae6
Teodoro Freund <tfreund95@gmail.com>
2026-03-19 11:19:29 +0000

    Removed some tests

--- crates/solidity-v2/testing/snapshots/cst_output/DecimalNumberExpression/decimal_trailing_unit_ether/input.sol
+++
@@ -1,8 +0,0 @@
-contract C {
-    function f() {
-// >>> Copied from crates/solidity/testing/snapshots/cst_output/DecimalNumberExpression/decimal_trailing_unit_ether/input.sol
-1ether
-// <<<
-;
-    }
-}
