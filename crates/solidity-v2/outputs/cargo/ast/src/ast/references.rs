367a1cea5c0bcfeb2cf2708bc91be4eb1ec866c2
Gustavo Giráldez <ggiraldez@manas.tech>
2026-04-01 17:33:08 -0400

    Migrate most of the AST API to v2; ABI computations still remain

---
+++ crates/solidity-v2/outputs/cargo/ast/src/ast/references.rs
@@ -0,0 +1,55 @@
+use std::rc::Rc;
+
+use slang_solidity_v2_ir::ir::{self, NodeId};
+use slang_solidity_v2_semantic::context::SemanticContext;
+
+use super::{create_identifier, Definition, Identifier};
+
+pub struct Reference {
+    identifier: Identifier,
+}
+
+impl Reference {
+    pub(crate) fn try_create(
+        ir_node: &ir::Identifier,
+        semantic: &Rc<SemanticContext>,
+    ) -> Option<Self> {
+        // ensure the terminal node is actually functioning as a reference
+        semantic
+            .binder()
+            .find_reference_by_identifier_node_id(ir_node.id())?;
+
+        Some(Self {
+            identifier: create_identifier(ir_node, semantic),
+        })
+    }
+
+    pub fn name(&self) -> String {
+        self.identifier.name()
+    }
+
+    pub fn resolve_to_definition(&self) -> Option<Definition> {
+        self.identifier.resolve_to_definition()
+    }
+
+    pub fn resolve_to_immediate_definition(&self) -> Option<Definition> {
+        self.identifier.resolve_to_immediate_definition()
+    }
+}
+
+pub(crate) fn references_binding_to_definition(
+    node_id: NodeId,
+    semantic: &Rc<SemanticContext>,
+) -> Vec<Reference> {
+    semantic
+        .binder()
+        .get_references_by_definition_id(node_id)
+        .iter()
+        .filter_map(|node_id| {
+            semantic
+                .binder()
+                .find_reference_by_identifier_node_id(*node_id)
+                .and_then(|reference| Reference::try_create(&reference.identifier, semantic))
+        })
+        .collect()
+}
--- crates/solidity-v2/outputs/cargo/semantic/src/context/mod.rs
+++ crates/solidity-v2/outputs/cargo/semantic/src/context/mod.rs
@@ -1,7 +1,7 @@
 use slang_solidity_v2_common::versions::LanguageVersion;
 use slang_solidity_v2_ir::ir::{self, NodeId};
 
-use crate::binder::Binder;
+use crate::binder::{Binder, Scope};
 use crate::passes::{
     p1_collect_definitions, p2_linearise_contracts, p3_type_definitions, p4_resolve_references,
 };
@@ -72,6 +72,14 @@ impl SemanticContext {
 }
 
 impl SemanticContext {
+    pub fn node_id_to_file_id(&self, node_id: NodeId) -> Option<String> {
+        let scope_id = self.binder().scope_id_for_node_id(node_id)?;
+        let Scope::File(file_scope) = self.binder().get_scope_by_id(scope_id) else {
+            return None;
+        };
+        Some(file_scope.file_id.clone())
+    }
+
     pub fn resolve_reference_identifier_to_definition_id(&self, node_id: NodeId) -> Option<NodeId> {
         let reference = self
             .binder()
