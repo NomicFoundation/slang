0cd403d8120583b0f95f856ec8467af194506c27
Teodoro Freund <tfreund95@gmail.com>
2026-04-01 20:17:59 +0000

    Added ranges to non terminals

--- crates/solidity-v2/outputs/cargo/tests/src/cst/cst_output/runner.rs
+++ crates/solidity-v2/outputs/cargo/tests/src/cst/cst_output/runner.rs
@@ -45,11 +45,9 @@ pub fn run(parser_name: &str, test_name: &str) -> Result<()> {
             // Note: comparing objects cheaply before expensive serialization.
             Some(ref last) if last == &v2_output => continue,
             _ => {
-                let (status, content) =  solidity_v2_testing_utils::cst_renderer::render(
-                        &source,
-                        &source_id,
-                        &v2_output,
-                    );
+                let (status, content) = solidity_v2_testing_utils::cst_renderer::render(
+                    &source, &source_id, &v2_output,
+                );
 
                 let snapshot_path = test_dir
                     .join("generated")
