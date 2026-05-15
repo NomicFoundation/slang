3f1a9e89f48f4bddb8c3e3d523870d5f364d6285
Gustavo Giráldez <ggiraldez@manas.tech>
2026-05-15 11:13:06 -0400

    Derive serialization for `NodeId`

--- crates/solidity-v2/outputs/cargo/common/src/nodes/mod.rs
+++ crates/solidity-v2/outputs/cargo/common/src/nodes/mod.rs
@@ -1,15 +1,12 @@
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+use serde::Serialize;
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
 #[repr(transparent)]
+#[serde(transparent)]
 pub struct NodeId(usize);
 
 impl From<usize> for NodeId {
     fn from(value: usize) -> Self {
         Self(value)
-    }
-}
-
-impl serde::Serialize for NodeId {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_u64(self.0 as u64)
     }
 }
