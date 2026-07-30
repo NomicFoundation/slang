d732d0a9a32b166e61b542cacd7073686d73d0ca
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2026-07-30 20:57:40 +0400

    [v2] feat(common): convert NodeId into usize

--- crates/solidity-v2/outputs/cargo/common/src/nodes/mod.rs
+++ crates/solidity-v2/outputs/cargo/common/src/nodes/mod.rs
@@ -13,6 +13,12 @@ impl From<usize> for NodeId {
     }
 }
 
+impl From<NodeId> for usize {
+    fn from(value: NodeId) -> Self {
+        value.0
+    }
+}
+
 impl fmt::Display for NodeId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
